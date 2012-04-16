a22f148c8c21c79e134b8201c26654adb3383e58
Fastspot <info@fastspot.com>
2012-04-16 14:22:26 -0400

    Fixed pop-list not saving sort order on first creation and a related warning.

--- core/admin/ajax/developer/load-table-columns.php
+++ core/admin/ajax/developer/load-table-columns.php
@@ -2,18 +2,27 @@
 	$field = $_GET["field"] ? $_GET["field"] : $_POST["field"];
 	$table = $_GET["table"] ? $_GET["table"] : $_POST["table"];
 	$sort = $_GET["sort"] ? $_GET["sort"] : $_POST["sort"];
-	$cols = sqlcolumns($table);
+	
+	if ($table) {
+		$cols = sqlcolumns($table);
 ?>
 <select name="<?=$field?>">
 	<option></option>
 	<?
 		foreach ($cols as $col) {
 			if ($sort) {
-				echo '<option>'.$col["name"].' asc</option>';
-				echo '<option>'.$col["name"].' desc</option>';
+				echo '<option>'.$col["name"].' ASC</option>';
+				echo '<option>'.$col["name"].' DESC</option>';
 			} else {
 				echo '<option>'.$col["name"].'</option>';
 			}		
 		}
 	?>
 </select>
+<?
+	} else {
+?>
+<small>-- Please select a table --</small>
+<?
+	}
+?>
