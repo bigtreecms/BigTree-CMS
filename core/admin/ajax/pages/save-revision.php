1071ff575bcfce7287b2fdb08628d4a2b42e6866
Tim Buckingham <mr@timbuckingham.com>
2012-05-31 11:27:07 -0400

    Fixed saving a revision not showing that any activity occurred.  Now growls and refreshes the page.

--- core/admin/ajax/pages/save-revision.php
+++ core/admin/ajax/pages/save-revision.php
@@ -1,7 +1,12 @@
 <?
+	header("Content-type: text/javascript");
+	
 	if (is_numeric($_POST["id"])) {
 		$admin->updatePageRevision($_POST["id"],$_POST["description"]);
 	} else {
 		$admin->saveCurrentPageRevision(substr($_POST["id"],1),$_POST["description"]);
 	}
+	
+	$admin->growl("Pages","Saved Revision");
 ?>
+window.location.reload();
