0e8781c76e9979ad940480089f52e0d21933c94c
Tim Buckingham <mr@timbuckingham.com>
2012-06-04 09:58:45 -0400

    Fixed 404s not being htmlspecialchar'd.

--- core/admin/ajax/dashboard/404/search.php
+++ core/admin/ajax/dashboard/404/search.php
@@ -8,7 +8,7 @@
 ?>
 <li>
 	<section class="requests_404"><?=$item["requests"]?></section>
-	<section class="url_404"><?=$item["broken_url"]?></section>
+	<section class="url_404"><?=htmlspecialchars($item["broken_url"])?></section>
 	<section class="redirect_404">
 		<input type="text" tabindex="<?=$tabindex?>" name="<?=$item["id"]?>" id="404_<?=$item["id"]?>" class="autosave" value="<?=htmlspecialchars($item["redirect_url"])?>" />
 	</section>
