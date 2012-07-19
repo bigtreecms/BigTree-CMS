588927a5002bf5425eb3e07fee7969dbb1c88014
Tim Buckingham <mr@timbuckingham.com>
2012-07-19 16:07:20 -0400

    Removed some unneeded code from the developer section.

--- core/admin/modules/developer/_header.php
+++ core/admin/modules/developer/_header.php
@@ -2,18 +2,6 @@
 	$module_title = "Developer";
 	$developer_root = ADMIN_ROOT."developer/";
 	$admin->requireLevel(2);
-	$autoModule = new BigTreeAutoModule;
-	
-	$subnav = array(
-		array("title" => "Templates", "link" => "developer/templates/"),
-		array("title" => "Modules", "link" => "developer/modules/"),
-		array("title" => "Callouts", "link" => "developer/callouts/"),
-		array("title" => "Field Types", "link" => "developer/field-types/"),
-		array("title" => "Feeds", "link" => "developer/feeds/"),
-		array("title" => "Settings", "link" => "developer/settings/"),
-		array("title" => "Upload Service", "link" => "developer/upload-service/"),
-		array("title" => "Site Status", "link" => "developer/status/"),
-	);
 	
 	$breadcrumb = array(
 		array("title" => "Developer", "link" => "developer/")
