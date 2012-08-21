f5527511268a5e6ebc4e7a3fe34cf6cf48b81ee2
Tim Buckingham <mr@timbuckingham.com>
2012-08-21 14:07:42 -0400

    Removed the code button from the default Simple HTML editor.

--- core/admin/layouts/_tinymce_specific_simple.php
+++ core/admin/layouts/_tinymce_specific_simple.php
@@ -11,7 +11,7 @@
 		elements: "<?=implode(",",$bigtree["simple_html_fields"])?>",
 		file_browser_callback: "BigTreeFileManager.tinyMCEOpen",
 		plugins: "inlinepopups,paste",
-		theme_advanced_buttons1: "link,unlink,bold,italic,underline,pasteword,code",
+		theme_advanced_buttons1: "link,unlink,bold,italic,underline,pasteword",
 		theme_advanced_buttons2: "",
 		theme_advanced_buttons3: "",
 		theme_advanced_disable: "cleanup,charmap",
