656e51b5b5432de947de837d55104acdf8c6829e
Tim Buckingham <mr@timbuckingham.com>
2012-09-17 14:05:53 -0400

    Improved the layout of the Site Integrity Check.

--- core/admin/modules/dashboard/vitals-statistics/integrity/default.php
+++ core/admin/modules/dashboard/vitals-statistics/integrity/default.php
@@ -3,11 +3,15 @@
 	<? include BigTree::path("admin/modules/dashboard/vitals-statistics/_jump.php"); ?>
 </h1>
 
-<p>The site integrity check will search your site for broken/dead links and alert you to their presence should they exist.</p>
-<p>You may choose either to include external links (links to other sites) or ignore them.<br />Including external links will significantly slow down the integrity check and may throw false positives.</p>
-<br />
-<p>
-	<a href="external/" class="button"><span class="icon_small icon_small_world"></span>Include External Links</a>
-	&nbsp;
-	<a href="internal/" class="button"><span class="icon_small icon_small_server"></span>Only Internal Links</a>
-</p>
+<div class="form_container">
+	<section>
+		<p>The site integrity check will search your site for broken/dead links and alert you to their presence should they exist.</p>
+		<p>Including external links will take <strong>significantly longer</strong> the integrity check and <strong>may throw false positives</strong>.</p>
+	</section>
+	<footer>
+		<a href="external/" class="button"><span class="icon_small icon_small_world"></span>Include External Links</a>
+		&nbsp;
+		<a href="internal/" class="button"><span class="icon_small icon_small_server"></span>Only Internal Links</a>
+	</footer>
+</div>
+
