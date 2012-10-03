bfff63df16dd1020f508022dab266f4667a25bd4
Tim Buckingham <mr@timbuckingham.com>
2012-10-03 15:02:14 -0400

    No longer tries to untranslate system settings. Fixed some failures in Google Analytics stuff.

--- core/admin/ajax/dashboard/analytics/get-management-profiles.php
+++ core/admin/ajax/dashboard/analytics/get-management-profiles.php
@@ -5,8 +5,10 @@
 	$profiles = $analytics->management_profiles->listManagementProfiles($_POST["account"], $_POST["property"]);
 	$response = array();
 	
-	foreach ($profiles->items as $item) {
-		$response[] = array("name" => $item->name, "id" => $item->id);
+	if (is_array($profiles->items)) {
+		foreach ($profiles->items as $item) {
+			$response[] = array("name" => $item->name, "id" => $item->id);
+		}
 	}
 	echo json_encode($response);
 ?>
