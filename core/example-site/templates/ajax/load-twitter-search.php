3c0c60690f9a4d6bddc692270ad9b12254b43fe3
Tim Buckingham <mr@timbuckingham.com>
2012-10-31 14:26:10 -0400

    Wrong search thing in example site.

--- core/example-site/templates/ajax/load-twitter-search.php
+++ core/example-site/templates/ajax/load-twitter-search.php
@@ -1,15 +1,15 @@
 <?
 	$twitterAPI = new BTXTwitterAPI;
-	$twitterTimeline = $twitterAPI->timeline($_GET["timeline"]);
+	$twitterTimeline = $twitterAPI->search($_GET["search"]);
 	if ($_GET["sidebar"]) {
-		$twitterTimeline = array_slice($twitterTimeline,0,4);
+		$twitterTimeline["results"] = array_slice($twitterTimeline["results"],0,4);
 	}
 	
-	foreach ($twitterTimeline as $tweet) {
+	foreach ($twitterTimeline["results"] as $tweet) {
 ?>
 <article class="tweet">
 	<p><?=$tweet["text"]?></p>
-	<strong><a href="http://www.twitter.com/<?=$_GET["timeline"]?>/">@<?=$_GET["timeline"]?></a> <?=$tweet["created"]?></strong>
+	<strong><a href="http://www.twitter.com/<?=$tweet["user"]?>/">@<?=$tweet["user"]?></a> <?=$tweet["created"]?></strong>
 </article>
 <? 
 	}
