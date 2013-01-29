91f0cd2b0a7998c87e0a2ec5ad0f190f8e839bf8
Tim Buckingham <mr@timbuckingham.com>
2013-01-29 14:07:04 -0500

    Fixed quick/advanced search returning pages you didn't have access to. Fixed always showing the Pages tab in advanced search even if there were no results. Fixed weird display if there were no results in advanced search.

--- core/admin/ajax/quick-search-results.php
+++ core/admin/ajax/quick-search-results.php
@@ -7,12 +7,15 @@
 		echo '<p>Quick Search Results</p>';
 		echo '<ul>';
 		foreach ($results as $r) {
-			$bc = $cms->getBreadcrumbByPage($r);
-			$crumbs = array();
-			foreach ($bc as $crumb) {
-				$crumbs[] = $crumb["title"];
+			$access_level = $admin->getPageAccessLevel($r["id"]);
+			if ($access_level) {
+				$bc = $cms->getBreadcrumbByPage($r);
+				$crumbs = array();
+				foreach ($bc as $crumb) {
+					$crumbs[] = $crumb["title"];
+				}
+				echo '<li><a href="'.ADMIN_ROOT."pages/view-tree/".$r["id"].'/" title="'.implode(" &raquo; ",$crumbs).'">'.$r["nav_title"].'</a></li>';
 			}
-			echo '<li><a href="'.ADMIN_ROOT."pages/view-tree/".$r["id"].'/" title="'.implode(" &raquo; ",$crumbs).'">'.$r["nav_title"].'</a></li>';
 		}
 		echo '</ul>';
 		echo '<a class="advanced_search" href="#">Advanced Search</a>';
