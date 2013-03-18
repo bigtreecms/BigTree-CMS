60c97e601c65cb1d4cd143c80f3a533e5e3a15dd
Tim Buckingham <mr@timbuckingham.com>
2013-03-17 22:21:00 -0400

    Fixed Feed Editor using the crappy version of a field adder and made it show the placeholder text for the parser.

--- core/admin/ajax/developer/load-feed-fields.php
+++ core/admin/ajax/developer/load-feed-fields.php
@@ -35,14 +35,7 @@
 	<label>Fields <small>(For RSS feeds, you do not need to manage this section.  Please edit the feed options to assign fields to RSS entries.)</small></label>
 
 	<div class="form_table">
-		<header>
-			<a href="#" class="add add_field"><span></span>Add</a>
-			<select id="unused_field" class="custom_control">
-				<? foreach ($unused as $field => $title) { ?>
-				<option value="<?=htmlspecialchars($title)?>"><?=htmlspecialchars($field)?></option>
-				<? } ?>
-			</select>
-		</header>
+		<header></header>
 		<div class="labels">
 			<span class="developer_view_title">Title</span>
 			<span class="developer_view_parser">Parser</span>
@@ -57,7 +50,7 @@
 					<input type="text" name="fields[<?=$key?>][title]" value="<?=$field["title"]?>" />
 				</section>
 				<section class="developer_view_parser">
-					<input type="text" name="fields[<?=$key?>][parser]" value="<?=$field["parser"]?>" />
+					<input type="text" name="fields[<?=$key?>][parser]" value="<?=$field["parser"]?>"  placeholder="PHP code to transform $value (which contains the column value.)" />
 				</section>
 				<section class="developer_resource_action">
 					<a href="#" class="icon_delete"></a>
@@ -72,50 +65,45 @@
 	var current_editing_key;
 	
 	$(".form_table").on("click",".icon_delete",function() {
-		new BigTreeDialog("Delete Field",'<p class="confirm">Are you sure you want to delete this field?</p>',$.proxy(function() {
-			tf = $(this).parents("li").find("section").find("input");
+		tf = $(this).parents("li").find("section").find("input");
 		
-			title = tf.val();
-			key = tf.attr("name").substr(7);
-			key = key.substr(0,key.length-8);
-			
-			sel = $("#unused_field").get(0);
-			sel.options[sel.options.length] = new Option(key,title,false,false);
-			$(this).parents("li").remove();
-		},this),"delete",false,"OK");
+		title = tf.val();
+		key = tf.attr("name").substr(7);
+		key = key.substr(0,key.length-8);
+		
+		fieldSelect.addField(key,title);
 
+		$(this).parents("li").remove();		
 		return false;
-	});
-		
+	});		
 	
-	$("#sort_table").sortable({ axis: "y", containment: "parent", handle: ".icon_sort", items: "li", placeholder: "ui-sortable-placeholder", tolerance: "pointer" });
-	
-	$("#field_area .add").click(function() {
-		un = $("#unused_field").get(0);
-		
-		if (un.selectedIndex > -1) {
-			key = un.options[un.selectedIndex].text;
-			title = un.options[un.selectedIndex].value;
+	function _local_hooks() {
+		$("#sort_table").sortable({ axis: "y", containment: "parent", handle: ".icon_sort", items: "li", placeholder: "ui-sortable-placeholder", tolerance: "pointer" });
+	}
+	_local_hooks();
 
-			li = $('<li id="row_' + key + '">');
-			li.html('<section class="developer_view_title"><span class="icon_sort"></span><input type="text" name="fields[' + key + '][title]" value="' + title + '" /></section><section class="developer_view_parser"><input type="text" class="parser" name="fields[' + key + '][parser]" value="" /></section><section class="developer_resource_action"><a href="#" class="icon_delete"></a></section>');
+	fieldSelect = new BigTreeFieldSelect(".form_table header",<?=json_encode($unused)?>,function(el,fs) {
+		title = el.title;
+		key = el.field;
 		
+		if (title) {
+			li = $('<li id="row_' + key + '">');
+			li.html('<section class="developer_view_title"><span class="icon_sort"></span><input type="text" name="fields[' + key + '][title]" value="' + title + '" /></section><section class="developer_view_parser"><input type="text" class="parser" name="fields[' + key + '][parser]" value="" placeholder="PHP code to transform $value (which contains the column value.)"/></section><section class="developer_resource_action"><a href="#" class="icon_delete"></a></section>');
+		
+			fs.removeCurrent();
 			$("#sort_table").append(li);
-			$("#sort_table").sortable({ items: "li", handle: ".icon_sort" });
-			un.remove(un.selectedIndex);
+			_local_hooks();
 		} else {
 			new BigTreeDialog("Add Custom Column",'<fieldset><label>Column Key <small>(must be unique)</small></label><input type="text" name="key" /></fieldset><fieldset><label>Column Title</label><input type="text" name="title" /></fieldset>',function(data) {
 				key = htmlspecialchars(data.key);
 				title = htmlspecialchars(data.title);
 				
 				li = $('<li id="row_' + key + '">');
-				li.html('<section class="developer_view_title"><span class="icon_sort"></span><input type="text" name="fields[' + key + '][title]" value="' + title + '" /></section><section class="developer_view_parser"><input type="text" class="parser" name="fields[' + key + '][parser]" value="" /></section><section class="developer_resource_action"><a href="#" class="icon_delete"></a></section>');
-		
+				li.html('<section class="developer_view_title"><span class="icon_sort"></span><input type="text" name="fields[' + key + '][title]" value="' + title + '" /></section><section class="developer_view_parser"><input type="text" class="parser" name="fields[' + key + '][parser]" value="" placeholder="PHP code to transform $value (which contains the column value.)" /></section><section class="developer_resource_action"><a href="#" class="icon_delete"></a></section>');
 				$("#sort_table").append(li);
-				$("#sort_table").sortable({ items: "li", handle: ".icon_sort" });
+				_local_hooks();
 			});
 		}
-		
-		return false;
 	});
+
 </script>
