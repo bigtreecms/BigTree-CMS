e2129e50cc72b101b3cf55e33e5c36313e2cd873
Tim Buckingham <mr@timbuckingham.com>
2013-04-25 22:39:25 -0400

    BACKWARDS COMPATIBILITY BREAK: Removed menu as a field type. Use Array of Items instead.

--- core/admin/form-field-types/draw/menu.php
+++
@@ -1,8 +0,0 @@
-<?
-	$field["options"]["fields"] = array(
-		array("title" => "Title", "key" => "title", "type" => "text"),
-		array("title" => "Link (include http://)", "key" => "link", "type" => "text")
-	);
-	
-	include BigTree::path("admin/form-field-types/draw/array.php");
-?>
--- core/admin/form-field-types/process/menu.php
+++
@@ -1,7 +0,0 @@
-<?
-	foreach ($data[$key] as &$item) {
-		$item = json_decode(str_replace(array(WWW_ROOT,STATIC_ROOT),array("{wwwroot}","{staticroot}"),$item),true);
-	}
-	
-	$value = $data[$key];
-?>
