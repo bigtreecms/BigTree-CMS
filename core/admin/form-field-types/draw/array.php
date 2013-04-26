79a55fe8eb851b2ef618470c601f5bbc22d59d86
Tim Buckingham <mr@timbuckingham.com>
2013-04-25 22:49:41 -0400

    Array of items.

--- core/admin/form-field-types/draw/array.php
+++ core/admin/form-field-types/draw/array.php
@@ -1,39 +1,34 @@
 <?
-	$entries = is_array($value) ? $value : array();
+	$entries = is_array($field["value"]) ? $field["value"] : array();
 	$x = 0;
-	
-	$clean_key = str_replace(array("[","]"),"_",$key);
 	
 	// We recreate the field array because Chrome doesn't like to do for loops properly when the numeric keys are out of order.
 	$aoi_fields = array();
-	foreach ($options["fields"] as $f) {
+	foreach ($field["options"]["fields"] as $f) {
 		$aoi_fields[] = $f;
 	}
 ?>
-<fieldset id="<?=$clean_key?>">
-	<? if ($title) { ?><label><?=$title?><? if ($subtitle) { ?> <small><?=$subtitle?></small><? } ?></label><? } ?>
-	<div class="multi_widget">
-		<ul>
-			<?
-				foreach ($entries as $entry) {
-			?>
-			<li>
-				<input type="hidden" name="<?=$key?>[<?=$x?>]" value="<?=htmlspecialchars(json_encode($entry))?>" />
-				<span class="icon_sort"></span>
-				<p><?=BigTree::trimLength(strip_tags(current($entry)),100)?></p>
-				<a href="#" class="icon_delete"></a>
-				<a href="#" class="icon_edit"></a>
-			</li>
-			<?
-					$x++;
-				}
-			?>
-		</ul>
-		<footer>
-			<a href="#" class="add button"><span class="icon_small icon_small_add"></span>Add Item</a>
-		</footer>
-	</div>
-</fieldset>
+<div class="multi_widget" id="<?=$field["id"]?>">
+	<ul>
+		<?
+			foreach ($entries as $entry) {
+		?>
+		<li>
+			<input type="hidden" name="<?=$field["key"]?>[<?=$x?>]" value="<?=htmlspecialchars(json_encode($entry))?>" />
+			<span class="icon_sort"></span>
+			<p><?=htmlspecialchars(htmlspecialchars_decode(BigTree::trimLength(strip_tags(current($entry)),100)))?></p>
+			<a href="#" class="icon_delete"></a>
+			<a href="#" class="icon_edit"></a>
+		</li>
+		<?
+				$x++;
+			}
+		?>
+	</ul>
+	<footer>
+		<a href="#" class="add button"><span class="icon_small icon_small_add"></span>Add Item</a>
+	</footer>
+</div>
 <script>
-	new BigTreeArrayOfItems("<?=$clean_key?>",<?=$x?>,"<?=$key?>",<?=json_encode($aoi_fields)?>);
+	new BigTreeArrayOfItems("<?=$field["id"]?>",<?=$x?>,"<?=$field["key"]?>",<?=json_encode($aoi_fields)?>);
 </script>
