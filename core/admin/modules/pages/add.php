963dab1f357dc2e797300bc848a0dd4bf7bce516
Tim Buckingham <mr@timbuckingham.com>
2013-05-14 09:45:21 -0400

    Fixed missing parent on creating pages.

--- core/admin/modules/pages/add.php
+++ core/admin/modules/pages/add.php
@@ -1,7 +1,7 @@
 <?
 	$tags = array();
 	$bigtree["form_action"] = "create";
-	$bigtree["current_page"] = array();
+	$bigtree["current_page"] = array("id" => $bigtree["current_page"]["id"]);
 	// Reset the $page variable to take out the information from the parent page.
 	$page = array("id" => $page["id"]);
 	include BigTree::path("admin/modules/pages/_form.php");
