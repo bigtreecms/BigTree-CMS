ccd82ce1e45a88f5bef7e545db2cdc2c1c1b6ead
Tim Buckingham <mr@timbuckingham.com>
2013-05-14 14:34:14 -0400

    Geocoding is broken!111

--- core/admin/form-field-types/process/geocoding.php
+++ core/admin/form-field-types/process/geocoding.php
@@ -1,23 +1,25 @@
 <?
 	// Loop through all the fields to build the address
-	$fields = explode(",",$field["options"]["fields"]);
+	$source_fields = explode(",",$field["options"]["fields"]);
 	$location = array();
-	foreach ($fields as $field) {
-		$location[] = $bigtree["post_data"][trim($field)];
+	foreach ($source_fields as $source_field) {
+		if (trim($source_field)) {
+			$location[] = $bigtree["post_data"][trim($source_field)];
+		}
 	}
 	
-	if (trim($location)) {
+	if (count($location)) {
 		// Geocode
 		$geocoder = new BigTreeGeocoding;
 		$result = $geocoder->geocode(implode(", ",$location));
 		
 		// If it's false, we didn't get anything.
 		if (!$result) {
-			$bigtree["parsed_data"]["latitude"] = false;
-			$bigtree["parsed_data"]["longitude"] = false;
+			$bigtree["entry"]["latitude"] = false;
+			$bigtree["entry"]["longitude"] = false;
 		} else {
-			$bigtree["parsed_data"]["latitude"] = $result["latitude"];
-			$bigtree["parsed_data"]["longitude"] = $result["longitude"];
+			$bigtree["entry"]["latitude"] = $result["latitude"];
+			$bigtree["entry"]["longitude"] = $result["longitude"];
 		}
 	}
 		
