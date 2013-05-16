3f97a73e28691844f5b0aa62cc7247e2e320c88a
Tim Buckingham <mr@timbuckingham.com>
2013-05-16 17:22:04 -0400

    Fixed callout resources asking to be the H1 / body copy for SEO ranking.

--- core/admin/modules/developer/callouts/_common-js.php
+++ core/admin/modules/developer/callouts/_common-js.php
@@ -24,7 +24,7 @@
 		key = $(this).attr("name");
 		BigTree.localCurrentFieldKey = key;
 		
-		$.ajax("<?=ADMIN_ROOT?>ajax/developer/load-field-options/", { type: "POST", data: { template: "true", type: $("#type_" + key).val(), data: $("#options_" + key).val() }, complete: function(response) {
+		$.ajax("<?=ADMIN_ROOT?>ajax/developer/load-field-options/", { type: "POST", data: { callout: "true", type: $("#type_" + key).val(), data: $("#options_" + key).val() }, complete: function(response) {
 			new BigTreeDialog("Field Options",response.responseText,function(data) {
 				$.ajax("<?=ADMIN_ROOT?>ajax/developer/save-field-options/?key=" + BigTree.localCurrentFieldKey, { type: "POST", data: data });
 			});
