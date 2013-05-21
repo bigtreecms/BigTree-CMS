c24a1d6e8ff1ae6693d3588c48c1a3e7b00bbbd2
Tim Buckingham <mr@timbuckingham.com>
2013-05-21 15:13:40 -0400

    Commented the list field type.

--- core/admin/form-field-types/draw/list.php
+++ core/admin/form-field-types/draw/list.php
@@ -3,6 +3,7 @@
 	$is_group_based_perm = false;
 	$list = array();
 
+	// Database populated list.
 	if ($field["options"]["list_type"] == "db") {
 		$list_table = $field["options"]["pop-table"];
 		$list_id = $field["options"]["pop-id"];
@@ -14,21 +15,25 @@
 			$db_error = true;
 		} else {
 			$q = sqlquery("SELECT `id`,`$list_title` FROM `$list_table` ORDER BY $list_sort");
-		
+			
+			// Check if we're doing module based permissions on this table.
 			if ($bigtree["module"] && $bigtree["module"]["gbp"]["enabled"] && $form["table"] == $bigtree["module"]["gbp"]["table"] && $key == $bigtree["module"]["gbp"]["group_field"]) {
 				$is_group_based_perm = true;
 				while ($f = sqlfetch($q)) {
+					// Find out whether the logged in user can access a given group, and if so, specify the access level.
 					$access_level = $admin->canAccessGroup($bigtree["module"],$f["id"]);
 					if ($access_level) {
 						$list[] = array("value" => $f["id"],"description" => $f[$list_title],"access_level" => $access_level);
 					}
 				}
+			// We're not doing module group based permissions, get a regular list.
 			} else {
 				while ($f = sqlfetch($q)) {
 					$list[] = array("value" => $f["id"],"description" => $f[$list_title]);
 				}
 			}
 		}
+	// State List
 	} elseif ($field["options"]["list_type"] == "state") {
 		foreach (BigTree::$StateList as $a => $s) {
 			$list[] = array(
@@ -36,6 +41,7 @@
 				"description" => $s
 			);
 		}
+	// Country List
 	} elseif ($field["options"]["list_type"] == "country") {
 		foreach (BigTree::$CountryList as $c) {
 			$list[] = array(
@@ -43,14 +49,17 @@
 				"description" => $c
 			);
 		}
+	// Static List
 	} else {
 		$list = $field["options"]["list"];
 	}
 
+	// If the table was deleted for a database populated list, throw an error.
 	if ($db_error) {
 ?>
 <p class="error_message">The table for this field no longer exists (<?=htmlspecialchars($list_table)?>).</p>
 <?
+	// Draw the list.
 	} else {
 		$class = array();
 		if ($is_group_based_perm) {
