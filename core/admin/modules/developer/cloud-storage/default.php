ef56f1e4fcb547e3afdf09b83381f39e2fcbf48e
Tim Buckingham <mr@timbuckingham.com>
2013-06-17 16:17:12 -0400

    Standardized the connected state for different things in the admin.

--- core/admin/modules/developer/cloud-storage/default.php
+++ core/admin/modules/developer/cloud-storage/default.php
@@ -1,26 +1,17 @@
 <?
 	$storage = new BigTreeStorage;
-	
-	if ($storage->Service == "s3") {
-		$currently = "Amazon S3";
-	} elseif ($storage->Service == "rackspace") {
-		$currently = "Rackspace Cloud Files";
-	} else {
-		$currently = "Local Storage";
-	}
 ?>
 <div class="table">
-	<summary><h2>Currently Using<small><?=$currently?></small></h2></summary>
 	<section>
-		<a class="box_select<? if ($currently == "Local Storage") { ?> active<? } ?>" href="local/">
+		<a class="box_select<? if ($storage->Service == "local") { ?> connected<? } ?>" href="local/">
 			<span class="local_storage"></span>
 			<p>Local Storage</p>
 		</a>
-		<a class="box_select<? if ($currently == "Amazon S3") { ?> active<? } ?>" href="amazon/">
+		<a class="box_select<? if ($storage->Service == "s3") { ?> connected<? } ?>" href="amazon/">
 			<span class="amazon"></span>
 			<p>Amazon S3</p>
 		</a>
-		<a class="box_select<? if ($currently == "Rackspace Cloud Files") { ?> active<? } ?>" href="rackspace/">
+		<a class="box_select<? if ($storage->Service == "rackspace") { ?> connected<? } ?>" href="rackspace/">
 			<span class="rackspace"></span>
 			<p>Rackspace Cloud Files</p>
 		</a>
--- core/admin/modules/developer/geocoding/default.php
+++ core/admin/modules/developer/geocoding/default.php
@@ -8,42 +8,27 @@
 		$admin->updateSettingValue("bigtree-internal-geocoding-service",array("service" => "google", "settings" => array()));
 	}
 	$gateway = $cms->getSetting("bigtree-internal-geocoding-service");
-
-	if ($gateway["service"] == "google") {
-		$currently = "Google";
-	} elseif ($gateway["service"] == "bing") {
-		$currently = "Bing";
-	} elseif ($gateway["service"] == "yahoo") {
-		$currently = "Yahoo";
-	} elseif ($gateway["service"] == "yahoo-boss") {
-		$currently = "Yahoo BOSS";
-	} elseif ($gateway["service"] == "mapquest") {
-		$currently = "MapQuest";
-	} else {
-		$currently = "Google";
-	}
 ?>
 <div class="table">
-	<summary><h2>Currently Using<small><?=$currently?></small></h2></summary>
 	<section>
 		<p>Choose a service below to configure your geocoder settings.</p>
-		<a class="box_select" href="<?=DEVELOPER_ROOT?>geocoding/google/">
+		<a class="box_select<? if ($gateway["service"] == "google" || !$gateway["service"]) { ?> connected<? } ?>" href="<?=DEVELOPER_ROOT?>geocoding/google/">
 			<span class="google"></span>
 			<p>Google</p>
 		</a>
-		<a class="box_select" href="<?=DEVELOPER_ROOT?>geocoding/bing/">
+		<a class="box_select<? if ($gateway["service"] == "bing") { ?> connected<? } ?>" href="<?=DEVELOPER_ROOT?>geocoding/bing/">
 			<span class="bing"></span>
 			<p>Bing</p>
 		</a>
-		<a class="box_select" href="<?=DEVELOPER_ROOT?>geocoding/yahoo/">
+		<a class="box_select<? if ($gateway["service"] == "yahoo") { ?> connected<? } ?>" href="<?=DEVELOPER_ROOT?>geocoding/yahoo/">
 			<span class="yahoo"></span>
 			<p>Yahoo</p>
 		</a>
-		<a class="box_select" href="<?=DEVELOPER_ROOT?>geocoding/mapquest/">
+		<a class="box_select<? if ($gateway["service"] == "mapquest") { ?> connected<? } ?>" href="<?=DEVELOPER_ROOT?>geocoding/mapquest/">
 			<span class="mapquest"></span>
 			<p>MapQuest</p>
 		</a>
-		<a class="box_select" href="<?=DEVELOPER_ROOT?>geocoding/yahoo-boss/">
+		<a class="box_select<? if ($gateway["service"] == "yahoo-boss") { ?> connected<? } ?>" href="<?=DEVELOPER_ROOT?>geocoding/yahoo-boss/">
 			<span class="yahooboss"></span>
 			<p>Yahoo BOSS</p>
 		</a>
