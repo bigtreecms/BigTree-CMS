01e07304cc073f0b86e7d86579f1eba3b5fcf9db
Tim Buckingham <mr@timbuckingham.com>
2013-07-01 18:21:35 -0400

    Hopefully finished up the Salesforce API.

--- core/inc/bigtree/apis/salesforce.php
+++ core/inc/bigtree/apis/salesforce.php
@@ -10,7 +10,7 @@
 		var $URL = false;
 		var $Settings = array();
 		var $Cache = true;
-		
+
 		/*
 			Constructor:
 				Sets up the Salesforce API connections.
@@ -148,6 +148,33 @@
 		}
 
 		/*
+			Function: cURL
+				Calls an authenticated API call via cURL instead of the OAuthClient library.
+				This is needed for some calls where the library hangs.
+
+			Parameters:
+				endpoint - The endpoint to hit
+				method - The HTTP method to use
+				content - The content to send
+				json - Whether the content is JSON or not, defaults to false
+
+			Returns:
+				Response body.
+		*/
+
+		function cURL($endpoint,$method,$content,$json = false) {
+			$headers = array("Authorization: Bearer ".$this->Settings["token"]);
+			if ($json) {
+				$headers[] = "Content-type: application/json";
+			}
+
+			return BigTree::cURL($this->URL.$endpoint,$content,array(
+				CURLOPT_CUSTOMREQUEST => $method,
+				CURLOPT_HTTPHEADER => $headers
+			));
+		}
+
+		/*
 			Function: getObject
 				Returns a Salesforce object for the given name.
 
@@ -218,9 +245,13 @@
 
 	/*
 		Class: BigTreeSalesforceObject
+			Emulates most of the methods from BigTreeModule for a Salesforce object type.
 	*/
 
 	class BigTreeSalesforceObject {
+
+		protected $API;
+		protected $QueryFieldNames;
 
 		/*
 			Constructor:
@@ -248,17 +279,382 @@
 			$this->Triggerable = $object->triggerable;
 			$this->Undeleteable = $object->undeleteable;
 			$this->Updateable = $object->updateable;
+			
+			// Find out the fields of this object.
+			$response = $this->API->call("sobjects/".$this->Name."/describe/");
+			if (!isset($response->name)) {
+				return false;
+			} 
+			$fields = array();
+			$field_names = array();
+			foreach ($response->fields as $f) {
+				$field = new stdClass;
+				if (count($f->picklistValues)) {
+					$field->AvailableValues = array();
+					foreach ($f->picklistValues as $v) {
+						$value = new stdClass;
+						if ($v->active) {
+							$value->Value = $v->value;
+							$value->Label = $v->label;
+							$value->IsDefault = $v->defaultValue;
+						}
+						$field->AvailableValues[] = $value;
+					}
+				}
+				$field->DefaultValue = $f->defaultValue;
+				$field->Label = $f->label;
+				$field->Length = $f->length;
+				$field->Name = $f->name;
+				$field->Type = $f->type;
+				$fields[] = $field;
+				$field_names[] = $f->name;
+			}
+			$this->Fields = $fields;
+			$this->QueryFieldNames = implode(",",$field_names);
 		}
 
 		/*
-			Function: describe
-				Completely describes the individual metadata at all levels for the specified object.
-				For example, this can be used to retrieve the fields, URLs, and child relationships for the Account object.
+			Function: add
+				Adds a record to Salesforce.
+			
+			Parameters:
+				keys - An array of column names to add
+				vals - An array of values for each of the columns
+
+			Returns:
+				A BigTreeSalesforceRecord object if successful.
 		*/
 
-		function describe() {
-			$response = $this->API->call("sobjects/".$this->Name."/describe/");
-			print_r($response);
+		function add($keys,$vals) {
+			$record = array();
+			foreach ($keys as $key) {
+				$record[$key] = current($vals);
+				next($vals);
+			}
+			$response = json_decode($this->API->cURL("sobjects/".$this->Name."/","POST",json_encode($record),true));
+			// Look for a response ID.
+			if (isset($response->id)) {
+				// Setup a dumb record and build it ourselves
+				$r = new stdClass;
+				foreach ($this->Fields as $field) {
+					$field_name = $field->Name;
+					$r->$field_name = isset($record[$field_name]) ? $record[$field_name] : "";
+				}
+				$r = new BigTreeSalesforceRecord($r,$this->API);
+				$r->ID = $response->id;
+				$r->Type = $this->Name;
+				$r->CreatedAt = $r->UpdatedAt = date("Y-m-d H:i:s");
+				return $r;				
+			} else {
+				$this->API->Errors[] = json_decode($response);
+				return false;
+			}
+		}
+
+		/*
+			Function: delete
+				Deletes a record from Salesforce.
+
+			Returns:
+				true if successful.
+		*/
+
+		function delete($id) {
+			// We're going to straight up cURL because the outh class is bogus and hangs on this call.
+			$response = $this->API->cURL("sobjects/".$this->Type."/$id","DELETE");
+			// If we have a response, there's an error.
+			if ($response) {
+				$this->API->Errors[] = json_decode($response);
+				return false;
+			}
+			return true;
+		}
+
+		/*
+			Function: get
+				Returns a single record with the given ID.
+
+			Parameters:
+				id - The "Id" field of the record in Salesforce
+
+			Returns:
+				A BigTreeSalesforceRecord object
+		*/
+
+		function get($id) {
+			$response = $this->API->call("sobjects/".$this->Name."/$id");
+			if (!$response) {
+				return false;
+			}
+			return new BigTreeSalesforceRecord($response,$this->API);
+		}
+
+		/*
+			Function: getAll
+				Returns all records of this object type.
+
+			Parameters:
+				order - The sort order (in SOSQL syntax, defaults to "Id ASC")
+			
+			Returns:
+				An array of BigTreeSalesforceRecord objects.
+		*/
+
+		function getAll($order = "Id ASC") {
+			return $this->query("SELECT ".$this->QueryFieldNames." FROM ".$this->Name." ORDER BY $order");
+		}
+
+		/*
+			Function: getMatching
+				Returns records that match the key/value pairs.
+
+			Parameters:
+				fields - Either a single field key or an array of field keys (if you pass an array you must pass an array for values as well)
+				values - Either a signle field value or an array of field values (if you pass an array you must pass an array for fields as well)
+				order - The sort order (in SOSQL syntax, defaults to "Id ASC")
+				limit - Max number of records to return, defaults to all
+			
+			Returns:
+				An array of BigTreeSalesforceRecord objects.
+		*/
+
+		function getMatching($fields,$values,$order = "Id ASC",$limit = false) {
+			if (!is_array($fields)) {
+				$where = "$fields = '".sqlescape($values)."'";
+			} else {
+				$x = 0;
+				while ($x < count($fields)) {
+					$where[] = $fields[$x]." = '".sqlescape($values[$x])."'";
+					$x++;
+				}
+				$where = implode(" AND ",$where);
+			}
+			if ($where) {
+				$query = "SELECT ".$this->QueryFieldNames." FROM ".$this->Name." WHERE $where ORDER BY $order";
+			} else {
+				$query = "SELECT ".$this->QueryFieldNames." FROM ".$this->Name." ORDER BY $order";
+			}
+			if ($limit) {
+				$query .= " LIMIT $limit";
+			}
+			return $this->query($query);
+		}
+
+		/*
+			Function: getPage
+				Returns a page of records.
+			
+			Parameters:
+				page - The page to return.
+				order - The sort order (in SOSQL syntax, defaults to "Id ASC")
+				perpage - The number of results per page (defaults to 15)
+				where - Optional SOSQL WHERE conditions
+			
+			Returns:
+				An array of BigTreeSalesforceRecord objects.
+		*/
+		
+		function getPage($page = 1,$order = "id ASC",$perpage = 15,$where = false) {
+			// Don't try for page 0
+			if ($page < 1) {
+				$page = 1;
+			}
+			
+			if ($where) {
+				$query = "SELECT ".$this->QueryFieldNames." FROM ".$this->Name." WHERE $where ORDER BY $order LIMIT $perpage OFFSET ".(($page - 1) * $perpage);
+			} else {
+				$query = "SELECT ".$this->QueryFieldNames." FROM ".$this->Name." ORDER BY $order LIMIT $perpage OFFSET ".(($page - 1) * $perpage);
+			}
+			return $this->query($query);
+		}
+
+		/*
+			Function: query
+				Perform a SOQL query.
+
+			Parameters:
+				query - A SOQL query string
+
+			Returns:
+				An array of BigTreeSalesforceRecord objects.
+		*/
+
+		function query($query) {
+			$response = $this->API->call("query/?q=".urlencode($query));
+			if (!isset($response->records)) {
+				return false;
+			}
+			$records = array();
+			foreach ($response->records as $record) {
+				$records[] = new BigTreeSalesforceRecord($record,$this->API);
+			}
+			return $records;
+		}
+
+		/*
+			Function: search
+				Returns an array of records from Salesforce that match the search query.
+			
+			Parameters:
+				query - A string to search for.
+				order - The SOQL sort order (defaults to "Id ASC")
+				limit - Max number of records to return, defaults to all
+			
+			Returns:
+				An array of BigTreeSalesforceRecord objects.
+		*/
+		
+		function search($query,$order = "Id ASC",$limit = false) {
+			$where = array();
+			$searchable_types = array("string","picklist","textarea","phone","url");
+			foreach ($this->Fields as $field) {
+				if (in_array($field->Type,$searchable_types) && $field->Name != "Description") {
+					$where[] = $field->Name." LIKE '%".addslashes($query)."%'";
+				}
+			}
+			if (!count($where)) {
+				return false;
+			}
+			$query = "SELECT ".$this->QueryFieldNames." FROM ".$this->Name." WHERE ".implode(" OR ",$where)." ORDER BY $order";
+			if ($limit) {
+				$query .= " LIMIT $limit";
+			}
+			return $this->query($query);
+		}
+
+		/*
+			Function: update
+				Updates an entry in Salesforce.
+			
+			Parameters:
+				id - The Id of the entry.
+				fields - Either a single column key or an array of column keys (if you pass an array you must pass an array for values as well)
+				values - Either a signle column value or an array of column values (if you pass an array you must pass an array for fields as well)
+		*/
+		
+		function update($id,$fields,$values) {
+			$record = array();
+			if (is_array($fields)) {
+				foreach ($fields as $key) {
+					$record[$key] = current($values);
+					next($values);
+				}
+			} else {
+				$record[$fields] = $values;
+			}
+			$response = $this->API->cURL("sobjects/".$this->Name."/$id","PATCH",json_encode($record),true);
+			// If we have a response, there's an error.
+			if ($response) {
+				$this->API->Errors[] = json_decode($response);
+				return false;
+			}
+			return true;
+		}
+	}
+
+	/*
+		Class: BigTreeSalesforceRecord
+	*/
+
+	class BigTreeSalesforceRecord {
+
+		protected $API;
+
+		/*
+			Constructor:
+				Creates a new BigTreeSalesforceRecord object.
+
+			Parameters:
+				record - Salesforce data
+				api - Reference to BigTreeSalesforceAPI class instance
+		*/
+
+		function __construct($record,&$api) {
+			$this->API = $api;
+			// Save this ahead of time to keep things alphabetized.
+			$this->Columns = $record;
+			$this->CreatedAt = date("Y-m-d H:i:s",strtotime($record->CreatedDate));
+			$this->CreatedBy = $record->CreatedById;
+			$this->ID = $record->Id;
+			$this->Type = $record->attributes->type;
+			$this->UpdatedAt = date("Y-m-d H:i:s",strtotime($record->LastModifiedDate));
+			$this->UpdatedBy = $record->LastModifiedById;
+			// Remove a bunch of columns we can't modify
+			unset($record->attributes);
+			unset($this->Columns->CreatedById);
+			unset($this->Columns->CreatedDate);
+			unset($this->Columns->Id);
+			unset($this->Columns->IsDeleted);
+			unset($this->Columns->JigsawCompanyId);
+			unset($this->Columns->LastActivityDate);
+			unset($this->Columns->LastModifiedDate);
+			unset($this->Columns->LastModifiedById);
+			unset($this->Columns->LastReferencedDate);
+			unset($this->Columns->LastViewedDate);
+			unset($this->Columns->MasterRecordId);
+			unset($this->Columns->SystemModstamp);
+		}
+
+		/*
+			Function: delete
+				Deletes the record from Salesforce.
+
+			Returns:
+				true if successful.
+		*/
+
+		function delete() {
+			// We're going to straight up cURL because the outh class is bogus and hangs on this call.
+			$response = $this->API->cURL("sobjects/".$this->Type."/".$this->ID,"DELETE");
+			// If we have a response, there's an error.
+			if ($response) {
+				$this->API->Errors[] = json_decode($response);
+				return false;
+			}
+			return true;
+		}
+
+		/*
+			Function: save
+				Saves changes made to the Columns property of this object back to Salesforce.
+		*/
+
+		function save() {
+			$response = $this->API->cURL("sobjects/".$this->Type."/".$this->ID,"PATCH",json_encode($this->Columns),true);
+			// If we have a response, there's an error.
+			if ($response) {
+				$this->API->Errors[] = json_decode($response);
+				return false;
+			}
+			return true;
+		}
+
+		/*
+			Function: update
+				Updates this entry in Salesforce.
+			
+			Parameters:
+				fields - Either a single column key or an array of column keys (if you pass an array you must pass an array for values as well)
+				values - Either a signle column value or an array of column values (if you pass an array you must pass an array for fields as well)
+		*/
+		
+		function update($fields,$values) {
+			$record = array();
+			if (is_array($fields)) {
+				foreach ($fields as $key) {
+					$record[$key] = current($values);
+					next($values);
+				}
+			} else {
+				$record[$fields] = $values;
+			}
+			$response = $this->API->cURL("sobjects/".$this->Type."/".$this->ID,"PATCH",json_encode($record),true);
+			// If we have a response, there's an error.
+			if ($response) {
+				$this->API->Errors[] = json_decode($response);
+				return false;
+			}
+			return true;
 		}
 	}
 ?>
