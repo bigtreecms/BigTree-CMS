b0461a086b9b7ebf3eec7316e58bd60e0a82518e
Tim Buckingham <mr@timbuckingham.com>
2013-09-04 16:34:49 -0400

    Yahoo BOSS geocoder tweaks.

--- core/admin/modules/developer/geocoding/yahoo-boss/default.php
+++ core/admin/modules/developer/geocoding/yahoo-boss/default.php
@@ -1,7 +1,24 @@
-<?
-	$api = new BigTreeYahooBOSSAPI;
-?>
 <div class="container">
+	<?
+		$geo = new BigTreeGeocoding;
+		$api = new BigTreeYahooBOSSAPI;
+		if ($api->Connected && $geo->Service == "yahoo-boss") {
+	?>
+	<section>
+		<p>The Yahoo BOSS Geocoder is connected.</p>
+	</section>
+	<?
+		} elseif ($api->Connected) {
+	?>
+	<section>
+		<p>Yahoo BOSS API services have already been configured but Yahoo BOSS is not currently the active geocoder.</p>
+	</section>
+	<footer>
+		<a href="<?=DEVELOPER_ROOT?>geocoding/yahoo-boss/switch/" class="button blue">Activate Yahoo BOSS Geocoder</a>
+	</footer>
+	<?
+		} else { 
+	?>
 	<form method="post" action="<?=DEVELOPER_ROOT?>geocoding/yahoo-boss/activate/">
 		<section>
 			<p>To use the Yahoo BOSS API you will need a Yahoo BOSS paid account. Yahoo BOSS provides commercial, per-query pricing (i.e. $6.00 per 1,000 queries). BigTree will cache properly geocoded addresses as to limit your query usage. To set up an account, follow the steps below:</p>
@@ -28,4 +45,7 @@
 			<input type="submit" class="button blue" value="Activate Yahoo BOSS Geocoder" />
 		</footer>
 	</form>
+	<?
+		}
+	?>
 </div>
