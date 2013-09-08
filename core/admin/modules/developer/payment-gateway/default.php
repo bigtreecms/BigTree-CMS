98b83f53990ee6b38dcd14b948b9f56d9eeeceb7
Tim Buckingham <mr@timbuckingham.com>
2013-09-08 17:10:35 -0400

    Fixed payment gateway overview.

--- core/admin/modules/developer/payment-gateway/default.php
+++ core/admin/modules/developer/payment-gateway/default.php
@@ -8,23 +8,24 @@
 		$admin->updateSettingValue("bigtree-internal-payment-gateway",array("service" => "", "settings" => array()));
 	}
 	$gateway = $cms->getSetting("bigtree-internal-payment-gateway");
+	$service = isset($gateway["service"]) ? $gateway["service"] : false;
 ?>
 <div class="table">
 	<section>
 		<p>Choose a service below to configure your payment gateway settings.</p>
-		<a class="box_select<? if ($gateway["service"] == "authorize.net") { ?> connected<? } ?>" href="authorize/">
+		<a class="box_select<? if ($service == "authorize.net") { ?> connected<? } ?>" href="authorize/">
 			<span class="authorize"></span>
 			<p>Authorize.Net</p>
 		</a>
-		<a class="box_select<? if ($gateway["service"] == "paypal") { ?> connected<? } ?>" href="paypal/">
+		<a class="box_select<? if ($service == "paypal") { ?> connected<? } ?>" href="paypal/">
 			<span class="paypal"></span>
 			<p>PayPal Payments Pro</p>
 		</a>
-		<a class="box_select<? if ($gateway["service"] == "payflow") { ?> connected<? } ?>" href="payflow/">
+		<a class="box_select<? if ($service == "payflow") { ?> connected<? } ?>" href="payflow/">
 			<span class="payflow"></span>
 			<p>PayPal Payflow Gateway</p>
 		</a>
-		<a class="box_select<? if ($gateway["service"] == "linkpoint") { ?> connected<? } ?>" href="linkpoint/">
+		<a class="box_select<? if ($service == "linkpoint") { ?> connected<? } ?>" href="linkpoint/">
 			<span class="linkpoint"></span>
 			<p>First Data / LinkPoint</p>
 		</a>
