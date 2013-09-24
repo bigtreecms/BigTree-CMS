e7461320efb3646b6bc950331b01fcd02b3defac
Tim Buckingham <mr@timbuckingham.com>
2013-09-24 12:32:35 -0400

    Namespace updates.

--- core/admin/auto-modules/views/draggable.php
+++ core/admin/auto-modules/views/draggable.php
@@ -9,7 +9,7 @@
 	<header>
 		<?
 			$x = 0;
-			foreach ($view["fields"] as $key => $field) {
+			foreach ($bigtree["view"]["fields"] as $key => $field) {
 				$x++;
 		?>
 		<span class="view_column" style="width: <?=$field["width"]?>px;"><?=$field["title"]?></span>
@@ -17,7 +17,7 @@
 			}
 		?>
 		<span class="view_status">Status</span>		
-		<span class="view_action" style="width: <?=(count($view["actions"]) * 40)?>px;"><? if (count($view["actions"]) > 1) { ?>Actions<? } ?></span>
+		<span class="view_action" style="width: <?=(count($bigtree["view"]["actions"]) * 40)?>px;"><? if (count($bigtree["view"]["actions"]) > 1) { ?>Actions<? } ?></span>
 	</header>
 	<ul id="sort_table">
 		<? include BigTree::path("admin/ajax/auto-modules/views/draggable.php") ?>
@@ -27,14 +27,14 @@
 <? include BigTree::path("admin/auto-modules/views/_common-js.php") ?>
 <script>
 	BigTree.localSearch = function() {
-		$("#sort_table").load("<?=ADMIN_ROOT?>ajax/auto-modules/views/draggable/", { view: <?=$view["id"]?>, search: $("#search").val() }, BigTree.localCreateSortable);
+		$("#sort_table").load("<?=ADMIN_ROOT?>ajax/auto-modules/views/draggable/", { view: <?=$bigtree["view"]["id"]?>, search: $("#search").val() }, BigTree.localCreateSortable);
 	};
 
 	BigTree.localCreateSortable = function() {
 		<? if ($permission == "p") { ?>
 		if ($("#search").val() == "") {
 			$("#sort_table").sortable({ axis: "y", containment: "parent", handle: ".icon_sort", items: "li", placeholder: "ui-sortable-placeholder", tolerance: "pointer", update: function() {
-				$.ajax("<?=ADMIN_ROOT?>ajax/auto-modules/views/order/", { type: "POST", data: { view: "<?=$view["id"]?>", sort: $("#sort_table").sortable("serialize") } });
+				$.ajax("<?=ADMIN_ROOT?>ajax/auto-modules/views/order/", { type: "POST", data: { view: "<?=$bigtree["view"]["id"]?>", sort: $("#sort_table").sortable("serialize") } });
 			}});
 		}
 		<? } ?>
