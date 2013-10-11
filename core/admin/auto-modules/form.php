eec006ffb26b2b02d6efce9110e9390ca5ed9308
Tim Buckingham <mr@timbuckingham.com>
2013-10-11 12:41:50 -0400

    Believe I have now fixed everything I previously broke.

--- core/admin/auto-modules/form.php
+++ core/admin/auto-modules/form.php
@@ -3,7 +3,7 @@
 	$bigtree["form_root"] = ADMIN_ROOT.$bigtree["module"]["route"]."/".$bigtree["module_action"]["route"]."/";
 
 	// Provide developers a nice handy link for edit/return of this form
-	$bigtree["developer_nav_links"][] = array("url" => ADMIN_ROOT."developer/modules/forms/edit/".$bigtree["form"]["id"]."/?return=front","class" => "icon_settings","title" => "Edit in Developer");
+	$bigtree["developer_nav_links"][] = array("url" => ADMIN_ROOT."developer/modules/forms/edit/".$bigtree["form"]["id"]."/?return=front","class" => "icon_settings_generic","title" => "Edit in Developer");
 	// Audit Trail link
 	if ($bigtree["edit_id"]) {
 		$bigtree["developer_nav_links"][] = array("url" => ADMIN_ROOT."developer/audit/search/?table=".$bigtree["form"]["table"]."&entry=".$bigtree["edit_id"],"class" => "icon_trail","title" => "Audit Trail");		
