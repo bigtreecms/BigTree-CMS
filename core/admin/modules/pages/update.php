ea80a182f2a8b03b36a227b03b2f48c8deb52078
Tim Buckingham <mr@timbuckingham.com>
2014-01-09 10:42:09 -0500

    4.0 branch fix: https://github.com/bigtreecms/BigTree-CMS/issues/102

--- core/admin/modules/pages/update.php
+++ core/admin/modules/pages/update.php
@@ -1,4 +1,9 @@
 <?
+	// Stop random hits to the update page
+	if (!isset($_POST["page"])) {
+		BigTree::redirect(ADMIN_ROOT."pages/view-tree/0/");
+	}
+
 	// See if we've hit post_max_size
 	if (!$_POST["_bigtree_post_check"]) {
 		$_SESSION["bigtree_admin"]["post_max_hit"] = true;
