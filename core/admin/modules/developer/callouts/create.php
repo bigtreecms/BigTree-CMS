86f757f030a76b00d0f2d7ff292e6fad401bc431
Tim Buckingham <mr@timbuckingham.com>
2014-01-20 11:46:37 -0500

    Fixed the lack of ID checking when creating callouts.

--- core/admin/modules/developer/callouts/create.php
+++ core/admin/modules/developer/callouts/create.php
@@ -1,6 +1,12 @@
 <?
-	$admin->createCallout($_POST["id"],$_POST["name"],$_POST["description"],$_POST["level"],$_POST["resources"],$_POST["display_field"],$_POST["display_default"]);
-	
+	// Let's see if the ID has already been used.
+	if ($admin->getCallout($_POST["id"])) {
+		$_SESSION["bigtree_admin"]["admin_saved"] = $_POST;
+		$_SESSION["bigtree_admin"]["admin_error"] = true;
+		BigTree::redirect($developer_root."callouts/add/");
+	}
+
+	$admin->createCallout($_POST["id"],$_POST["name"],$_POST["description"],$_POST["level"],$_POST["resources"],$_POST["display_field"],$_POST["display_default"]);	
 	$admin->growl("Developer","Created Callout");
 	BigTree::redirect($developer_root."callouts/");
 ?>
