c65cf698d1eab47ed14c3494784c492b2a8d458a
Tim Buckingham <mr@timbuckingham.com>
2014-05-05 11:56:49 -0400

    Fixed "Advanced" htaccess so it works with Apache 2.4+

--- install.php
+++ install.php
@@ -392,34 +392,33 @@
 		
 		if ($routing == "advanced") {
 			bt_touch_writable("site/.htaccess",'<IfModule mod_deflate.c>
-  # force deflate for mangled headers developer.yahoo.com/blogs/ydn/posts/2010/12/pushing-beyond-gzipping/
-  <IfModule mod_setenvif.c>
-	<IfModule mod_headers.c>
-	  SetEnvIfNoCase ^(Accept-EncodXng|X-cept-Encoding|X{15}|~{15}|-{15})$ ^((gzip|deflate)\s,?\s(gzip|deflate)?|X{4,13}|~{4,13}|-{4,13})$ HAVE_Accept-Encoding
-	  RequestHeader append Accept-Encoding "gzip,deflate" env=HAVE_Accept-Encoding
+	<IfModule mod_setenvif.c>
+		<IfModule mod_headers.c>
+			SetEnvIfNoCase ^(Accept-EncodXng|X-cept-Encoding|X{15}|~{15}|-{15})$ ^((gzip|deflate)\s*,?\s*)+|[X~-]{4,13}$ HAVE_Accept-Encoding
+			RequestHeader append Accept-Encoding "gzip,deflate" env=HAVE_Accept-Encoding
+		</IfModule>
 	</IfModule>
-  </IfModule>
-  
-  # html, txt, css, js, json, xml, htc:
-  <IfModule filter_module>
-   FilterDeclare   COMPRESS
-   FilterProvider  COMPRESS  DEFLATE resp=Content-Type /text/(html|css|javascript|plain|x(ml|-component))/
-   FilterProvider  COMPRESS  DEFLATE resp=Content-Type /application/(javascript|json|xml|x-javascript)/
-   FilterChain	 COMPRESS
-   FilterProtocol  COMPRESS  change=yes;byteranges=no
- </IfModule>
- 
- # Legacy versions of Apache
- <IfModule !mod_filter.c>
-   AddOutputFilterByType DEFLATE text/html text/plain text/css application/json
-   AddOutputFilterByType DEFLATE text/javascript application/javascript application/x-javascript 
-   AddOutputFilterByType DEFLATE text/xml application/xml text/x-component
- </IfModule>
- 
- # webfonts and svg:
- <FilesMatch "\.(ttf|otf|eot|svg)$">
-   SetOutputFilter DEFLATE
- </FilesMatch>
+
+	<IfModule mod_filter.c>
+		AddOutputFilterByType DEFLATE 	application/atom+xml \
+										application/javascript \
+										application/json \
+										application/ld+json \
+										application/rss+xml \
+										application/vnd.ms-fontobject \
+										application/x-font-ttf \
+										application/x-web-app-manifest+json \
+										application/xhtml+xml \
+										application/xml \
+										font/opentype \
+										image/svg+xml \
+										image/x-icon \
+										text/css \
+										text/html \
+										text/plain \
+										text/x-component \
+										text/xml
+	</IfModule>
 </IfModule>
 
 <IfModule mod_expires.c>
