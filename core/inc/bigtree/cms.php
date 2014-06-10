e4f8767313080db32daaa188e41c109b64f7eb6e
Tim Buckingham <mr@timbuckingham.com>
2014-06-10 16:21:22 -0400

    Fixed warning throwing if it's not an array yet.

--- core/inc/bigtree/cms.php
+++ core/inc/bigtree/cms.php
@@ -93,8 +93,10 @@
 				}
 				if ($return_object) {
 					$obj = new stdClass;
-					foreach ($data as $key => $val) {
-						$obj->$key = $val;
+					if (is_array($data)) {
+						foreach ($data as $key => $val) {
+							$obj->$key = $val;
+						}
 					}
 					$this->AutoSaveSettings[$id] = $obj;
 				} else {
