af9f49987536a34db95393739c2eccaf7e3aa8e6
Tim Buckingham <mr@timbuckingham.com>
2014-06-24 21:03:47 -0400

    Fixed the cron file including BigTree 4.0-style configs. Fixed missing selected state for image browser.

--- core/cron.php
+++ core/cron.php
@@ -1,6 +1,7 @@
 <?
 	$server_root = str_replace("core/cron.php","",strtr(__FILE__, "\\", "/"));
-	include $server_root."templates/config.php";
+	include $server_root."custom/environment.php";
+	include $server_root."custom/settings.php";
 	include $server_root."core/bootstrap.php";
 	
 	if (BIGTREE_CUSTOM_ADMIN_CLASS) {
