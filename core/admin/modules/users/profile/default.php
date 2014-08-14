d24ad5fb6137508df645e70a592ec804eac9679a
Tim Buckingham <mr@timbuckingham.com>
2014-08-14 16:02:06 -0400

    Password policies for self editing passwords.

--- core/admin/modules/users/profile/default.php
+++ core/admin/modules/users/profile/default.php
@@ -2,18 +2,30 @@
 	$user = $admin->getUser($admin->ID);
 	$bigtree["gravatar"] = $user["email"];
 	BigTree::globalizeArray($user,array("htmlspecialchars"));
+
+	$error = false;
+	if (isset($_SESSION["bigtree_admin"]["update_profile"])) {
+		BigTree::globalizeArray($_SESSION["bigtree_admin"]["update_profile"],array("htmlspecialchars"));
+		$daily_digest = isset($daily_digest) ? $daily_digest : false;
+		unset($_SESSION["bigtree_admin"]["update_profile"]);
+		$error = true;
+	}
 ?>
 <div class="container">
 	<form class="module" action="<?=ADMIN_ROOT?>users/profile/update/" method="post">
 		<section>
+			<p class="error_message"<? if (!$error) { ?> style="display: none;"<? } ?>>Errors found! Please fix the highlighted fields before submitting.</p>
 			<div class="left">
 				<fieldset>
 					<label>Name</label>
 					<input type="text" name="name" value="<?=$name?>" tabindex="1" />
 				</fieldset>
-				<fieldset>
-					<label>Password <small>(leave blank to remain unchanged)</small></label>
-					<input type="password" name="password" value="" tabindex="3" autocomplete="off" />
+				<fieldset<? if ($error) { ?> class="form_error"<? } ?>>
+					<label>Password <small>(leave blank to remain unchanged)</small> <? if ($error) { ?><span class="form_error_reason">Did Not Meet Requirements</span><? } ?></label>
+					<input type="password" name="password" value="" tabindex="3" autocomplete="off" <? if ($policy) { ?> class="has_tooltip" data-tooltip="<?=htmlspecialchars($policy_text)?>"<? } ?> />
+					<? if ($policy) { ?>
+					<p class="password_policy">Password Policy In Effect</p>
+					<? } ?>
 				</fieldset>
 			</div>
 			<div class="right">
@@ -35,3 +47,6 @@
 		</footer>
 	</form>
 </div>
+<script>
+	BigTreePasswordInput("input[type=password]");
+</script>
--- core/admin/modules/users/profile/update.php
+++ core/admin/modules/users/profile/update.php
@@ -1,4 +1,9 @@
 <?
+	if ($_POST["password"] && !$admin->validatePassword($_POST["password"])) {
+		$_SESSION["bigtree_admin"]["update_profile"] = $_POST;
+		$admin->growl("Users","Invalid Password","error");
+		BigTree::redirect(ADMIN_ROOT."users/profile/");	
+	}
 	$admin->updateProfile($_POST);
 	$admin->growl("Users","Updated Profile");
 	BigTree::redirect(ADMIN_ROOT."dashboard/");
