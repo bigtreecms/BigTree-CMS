3c0e468613665767d0d49d797be52305be948b40
Tim Buckingham <mr@timbuckingham.com>
2014-09-24 14:41:42 -0400

    Preset deletion.

---
+++ core/admin/ajax/developer/media/delete-preset.php
@@ -0,0 +1,10 @@
+<?
+	$admin->requireLevel(2);
+	
+	// Get existing presets
+	$settings = $cms->getSetting("bigtree-internal-media-settings");
+
+	// Delete one of them
+	unset($settings["presets"][$_POST["id"]]);
+	$admin->updateSettingValue("bigtree-internal-media-settings",$settings);
+?>
