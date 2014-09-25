199a6d4004ccad857f6eb7b4bb12928520eb80fa
Tim Buckingham <mr@timbuckingham.com>
2014-09-25 10:54:36 -0400

    Too many slashes in that example.

--- core/config.environment.php
+++ core/config.environment.php
@@ -4,7 +4,7 @@
 
 	// Website Environment
 	$bigtree["config"]["debug"] = true; // Set to false to stop all PHP errors/warnings from showing, or "full" to show all errors include notices and strict standards
-	$bigtree["config"]["domain"] = "[domain]";	// "domain" should be http:///www.website.com
+	$bigtree["config"]["domain"] = "[domain]";	// "domain" should be http://www.website.com
 	$bigtree["config"]["www_root"] = "[wwwroot]"; // "www_root" should be http://www.website.com/location/of/the/site/
 	$bigtree["config"]["static_root"] = "[staticroot]"; // "static_root" can either be the same as "www_root" or another domain that points to the same place -i t is used to server static files to increase page load time due to max connections per domain in most browsers.
 	$bigtree["config"]["admin_root"] = "[wwwroot]admin/"; // "admin_root" should be the location you want to access BigTree's admin from, i.e. http://www.website.com/admin/
