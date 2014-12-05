6678fdc589e5d899229bf62b6679d5c0e296ac64
Tim Buckingham <mr@timbuckingham.com>
2014-12-05 14:41:45 -0500

    Doc update.

--- core/admin/modules/developer/extensions/build/default.php
+++ core/admin/modules/developer/extensions/build/default.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<section>
 		<h3>Extensions vs. Packages</h3>
-		<p><strong>Extensions</strong> are self-contained in their own directories. They can not install files that live outside the extension directory. This allows them to easily be installed on any BigTree installation without worrying about colliding with other BigTree components. Extensions can be automatically upgraded through BigTree and can be uninstalled.</p>
+		<p><strong>Extensions</strong> are self-contained in their own directories. They can not install files that live outside the extension directory. This allows them to easily be installed on any BigTree installation without worrying about colliding with other BigTree components. Extensions can be automatically upgraded through BigTree and can be uninstalled. When creating an extension from existing assets (modules, field types, templates), they will be moved into the /extensions/ directory and may need to have their calls adjusted to meet the guidelines below.</p>
 		<p><strong>Packages</strong> install into the global component system and can install files into any directory. They are meant to be installed in a controlled manner in a development environment. They are not updatable but can be uninstalled.</p>
 		<hr />
 		<h3>Extension Guidelines</h3>
