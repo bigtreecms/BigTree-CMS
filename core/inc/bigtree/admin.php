670b353c10ad62e87983cb835395087f2825f991
Tim Buckingham <mr@timbuckingham.com>
2015-02-18 13:48:56 -0500

    Fixed module designer's field options failing.

--- core/inc/bigtree/admin.php
+++ core/inc/bigtree/admin.php
@@ -1005,7 +1005,7 @@
 					"type" => BigTree::safeEncode($data["type"]),
 					"title" => BigTree::safeEncode($data["title"]),
 					"subtitle" => BigTree::safeEncode($data["subtitle"]),
-					"options" => (array)@json_decode($data["options"],true)
+					"options" => !is_array($data["options"]) ? (array)@json_decode($data["options"],true) : $data["options"]
 				);
 				// Backwards compatibility with BigTree 4.1 package imports
 				foreach ($data as $k => $v) {
