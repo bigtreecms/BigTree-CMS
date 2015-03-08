de57562875389dd2dc457a66265dbc9deb3f986c
Tim Buckingham <mr@timbuckingham.com>
2015-03-08 17:08:53 -0400

    Fixed cross-site request forgery vulnerability. Thanks to Charlie Clark for finding this!

--- core/admin/modules/users/profile/update.php
+++ core/admin/modules/users/profile/update.php
@@ -1,5 +1,15 @@
 <?
-	$admin->updateProfile($_POST);
-	$admin->growl("Users","Updated Profile");
-	BigTree::redirect(ADMIN_ROOT."dashboard/");
+	if ($_SERVER["HTTP_REFERER"] != ADMIN_ROOT."users/profile/") {
+?>
+<div class="container">
+	<section>
+		<p>To update your profile, please access your  <a href="<?=ADMIN_ROOT?>users/profile/">Profile</a> page directly.</p>
+	</section>
+</div>
+<?
+	} else {
+		$admin->updateProfile($_POST);
+		$admin->growl("Users","Updated Profile");
+		BigTree::redirect(ADMIN_ROOT."dashboard/");
+	}
 ?>
