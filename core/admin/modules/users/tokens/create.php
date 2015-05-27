783b77dcb7a4709f94fc08f8197d50489c348499
Tim Buckingham <mr@timbuckingham.com>
2015-05-27 17:33:50 -0400

    This stuff shouldn't exist anymore.

--- core/admin/modules/users/tokens/create.php
+++
@@ -1,8 +0,0 @@
-<?
-	$admin->requireLevel(2);
-	$admin->createAPIToken($_POST["user"],$_POST["read_only"]);
-	$admin->growl("Users","Added API Token");
-	
-	header("Location: ".$admin_root."users/tokens/");
-	die();
-?>
