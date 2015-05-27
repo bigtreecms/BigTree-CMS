3c24bd5f7ee28d7e3ebcc1aea2a36258594fa103
Tim Buckingham <mr@timbuckingham.com>
2015-05-27 16:42:31 -0400

    SQL injection fixes.

--- core/inc/bigtree/auto-modules.php
+++ core/inc/bigtree/auto-modules.php
@@ -716,7 +716,7 @@
 				return self::getPendingItem($table,$id);
 			}
 			// Otherwise it's a live entry
-			$item = sqlfetch(sqlquery("SELECT * FROM `$table` WHERE id = '$id'"));
+			$item = sqlfetch(sqlquery("SELECT * FROM `$table` WHERE id = '".sqlescape($id)."'"));
 			if (!$item) {
 				return false;
 			}
@@ -797,7 +797,7 @@
 			$owner = false;
 			// The entry is pending if there's a "p" prefix on the id
 			if (substr($id,0,1) == "p") {
-				$change = sqlfetch(sqlquery("SELECT * FROM bigtree_pending_changes WHERE id = '".substr($id,1)."'"));
+				$change = sqlfetch(sqlquery("SELECT * FROM bigtree_pending_changes WHERE id = '".sqlescape(substr($id,1))."'"));
 				if (!$change) {
 					return false;
 				}
@@ -815,7 +815,7 @@
 				$owner = $change["user"];
 			// Otherwise it's a live entry
 			} else {
-				$item = sqlfetch(sqlquery("SELECT * FROM `$table` WHERE id = '$id'"));
+				$item = sqlfetch(sqlquery("SELECT * FROM `$table` WHERE id = '".sqlescape($id)."'"));
 				if (!$item) {
 					return false;
 				}
@@ -1192,7 +1192,7 @@
 				$id = $id["id"];
 			}
 			
-			$view = sqlfetch(sqlquery("SELECT * FROM bigtree_module_views WHERE id = '$id'"));
+			$view = sqlfetch(sqlquery("SELECT * FROM bigtree_module_views WHERE id = '".sqlescape($id)."'"));
 			if (!$view) {
 				return false;
 			}
