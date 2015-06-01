7e0b5755d07e05efb477f7edd9b57e1da4e9f14f
Tim Buckingham <mr@timbuckingham.com>
2015-06-01 10:41:17 -0400

    Look, no wrapper!

--- core/admin/modules/developer/modules/views/_js.php
+++ core/admin/modules/developer/modules/views/_js.php
@@ -11,16 +11,15 @@
 			return;
 		}
 
-		$.ajax("<?=ADMIN_ROOT?>ajax/developer/load-view-options/", { type: "POST", data: { table: $("#view_table").val(), type: $("#view_type").val(), data: $("#view_options").val() }, complete: function(response) {
-			BigTreeDialog({
-				title: "View Options",
-				content: response.responseText,
-				icon: "edit",
-				callback: function(data) {
-					$("#view_options").val(JSON.stringify(data));
-				}
-			});
-		}});
+		BigTreeDialog({
+			url: "<?=ADMIN_ROOT?>ajax/developer/load-view-options/",
+			post: { table: $("#view_table").val(), type: $("#view_type").val(), data: $("#view_options").val() },
+			title: "View Options",
+			icon: "edit",
+			callback: function(data) {
+				$("#view_options").val(JSON.stringify(data));
+			}
+		});
 	});
 	
 	$("#view_type").change(function(event,data) {
