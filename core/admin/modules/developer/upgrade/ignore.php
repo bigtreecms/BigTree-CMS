d9c3946f526e711b21dbb911a5bd81ff93aeb21a
Tim Buckingham <mr@timbuckingham.com>
2015-06-01 10:40:59 -0400

    I broke ignoring updates in the 4.2.2 release... so you can't ignore upgrading to 4.2.3 until you upgrade to 4.2.3. Whoops.

--- core/admin/modules/developer/upgrade/ignore.php
+++ core/admin/modules/developer/upgrade/ignore.php
@@ -2,7 +2,7 @@
 	$versions = json_decode($_GET["versions"],true);
 	foreach ($versions as $version) {
 		// Prevent cookie screwage
-		$version = str_replace(array("["],"]"),"",$version);
+		$version = str_replace(array("[","]"),"",$version);
 		setcookie("bigtree_admin[ignored_update][$version]",true,time()+365*60*60*24,str_replace(DOMAIN,"",WWW_ROOT));
 	}
 	$admin->growl("Developer","Ignored Updates");
