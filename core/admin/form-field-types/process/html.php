7b84b8f688158738a733ae57f2656c63c7038d3d
Tim Buckingham <mr@timbuckingham.com>
2015-06-01 14:02:26 -0400

    Create function is apparently bad form. Don't need it in PHP 5.3 anyway.

--- core/admin/form-field-types/process/html.php
+++ core/admin/form-field-types/process/html.php
@@ -1,6 +1,7 @@
 <?php
-	// If there are admin links, we want them stripped out and returned back to relative URLs.
-	$field["output"] = preg_replace_callback('/href="([^"]*)"/',create_function('$matches','
+	$fix_referer = function($matches) {
 		$href = str_replace($_SERVER["HTTP_REFERER"],"",$matches[1]);
-		return \'href="\'.$href.\'"\';'
-	),$field["input"]);
+		return 'href="'.$href.'"';
+	};
+	// If there are admin links, we want them stripped out and returned back to relative URLs.
+	$field["output"] = preg_replace_callback('/href="([^"]*)"/',$fix_referer,$field["input"]);
