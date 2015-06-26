6cb83e1fb8eef2cc1ee6263031f98c05421f933a
Tim Buckingham <mr@timbuckingham.com>
2015-06-26 13:46:40 -0400

    Fixed draggable view reordering giving negative numbers.

--- core/admin/ajax/auto-modules/views/order.php
+++ core/admin/ajax/auto-modules/views/order.php
@@ -10,10 +10,10 @@
 	
 		foreach ($data["row"] as $position => $id) {
 			if (is_numeric($id)) {
-				sqlquery("UPDATE `$table` SET position = '".(count($row)-$position)."' WHERE id = '".sqlescape($id)."'");
+				sqlquery("UPDATE `$table` SET position = '".(count($data["row"]) - $position)."' WHERE id = '".sqlescape($id)."'");
 				BigTreeAutoModule::recacheItem($id,$table);
 			} else {
-				BigTreeAutoModule::updatePendingItemField(substr($id,1),"position",(count($row)-$position));
+				BigTreeAutoModule::updatePendingItemField(substr($id,1),"position",(count($data["row"]) - $position));
 				BigTreeAutoModule::recacheItem(substr($id,1),$table,true);
 			}
 		}
