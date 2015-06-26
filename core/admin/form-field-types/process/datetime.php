48ed07274e71c0bcc59e23864df26d3a09f0d94b
Tim Buckingham <mr@timbuckingham.com>
2015-06-26 13:26:55 -0400

    Fixed callout/matrix based date/time pickers losing their value on save.

--- core/admin/form-field-types/process/datetime.php
+++ core/admin/form-field-types/process/datetime.php
@@ -1,5 +1,10 @@
 <?
 	$date = DateTime::createFromFormat($bigtree["config"]["date_format"]." h:i a",$field["input"]);
+	// Fallback to SQL standards for existing values
+	if (!$date) {
+		$date = DateTime::createFromFormat("Y-m-d H:i:s",$field["input"]);
+	}
+
 	if ($date) {
 		$field["output"] = $date->format("Y-m-d H:i:s");
 	} else {
