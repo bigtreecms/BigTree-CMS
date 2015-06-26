745be9948f2a3c8c78e66b3860019188401b8cd7
Tim Buckingham <mr@timbuckingham.com>
2015-06-26 15:45:28 -0400

    I have no idea what edit-module-setting and update-module-setting are, but they apparently haven't been used since before 4.0 launched.

--- core/admin/modules/settings/default.php
+++ core/admin/modules/settings/default.php
@@ -1,39 +1,29 @@
-<?php $admin->requireLevel(1) ?>
-<div class="table">
-	<summary>
-		<input type="search" name="query" id="query" placeholder="Search" class="form_search" autocomplete="off" />
-		<span class="form_search_icon"></span>
-		<nav id="view_paging" class="view_paging"></nav>
-	</summary>
-	<header>
-		<span class="settings_name">Name</span>
-		<span class="settings_value">Value</span>
-		<span class="view_action">Edit</span>
-	</header>
-	<ul id="results">
-		<?php include BigTree::path("admin/ajax/settings/get-page.php") ?>	
-	</ul>
-</div>
-
-<script>
-	BigTree.localSearchTimer = false;
-	BigTree.localSearch = function() {
-		$("#results").load("<?=ADMIN_ROOT?>ajax/settings/get-page/?page=1&query=" + escape($("#query").val()));
-	};
+<?php
+	$admin->requireLevel(1);
 	
-	$("#query").keyup(function() {
-		if (BigTree.localSearchTimer) {
-			clearTimeout(BigTree.localSearchTimer);
+	$settings = $admin->getSettings();
+	foreach ($settings as &$item) {
+		if (is_array($item["value"]) || ($item["value"] && !strlen(trim(strip_tags($item["value"]))))) {
+			$item["value"] = "&mdash; Edit To View &mdash;";
+		} else {
+			$item["value"] = BigTree::trimLength(strip_tags($item["value"]),100);
 		}
-		BigTree.localSearchTimer = setTimeout("BigTree.localSearch()",400);
-	});
-
-	$(".table").on("click","#view_paging a",function() {
-		if ($(this).hasClass("active") || $(this).hasClass("disabled")) {
-			return false;
-		}
-		$("#results").load("<?=ADMIN_ROOT?>ajax/settings/get-page/?page=" + BigTree.cleanHref($(this).attr("href")) + "&query=" + escape($("#query").val()));
-
-		return false;
+	}
+?>
+<div id="settings_table"></div>
+<script>
+	BigTreeTable({
+		container: "#settings_table",
+		columns: {
+			name: { title: "Name", sort: "asc", size: 0.3 },
+			value: { title: "Value" }
+		},
+		actions: {
+			edit: "<?=ADMIN_ROOT?>settings/edit/{id}/"
+		},
+		data: <?=BigTree::jsonExtract($settings,array("id","name","value"))?>,
+		searchable: true,
+		sortable: true,
+		perPage: 10
 	});
 </script>
