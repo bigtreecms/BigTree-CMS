2e82c63e9329b0e86a290ed370ddd7cfc9283629
Tim Buckingham <mr@timbuckingham.com>
2015-08-21 11:14:52 -0400

    Fixed extensions that check for some internal settings inadvertantly creating their own extension namespaced copies if the settings didn't yet exist.

--- core/inc/bigtree/cms.php
+++ core/inc/bigtree/cms.php
@@ -79,7 +79,7 @@
 				// Create a setting if it doesn't exist yet
 				if ($data === false) {
 					// If an extension is creating an auto save setting, make it a reference back to the extension
-					if (defined("EXTENSION_ROOT")) {
+					if (defined("EXTENSION_ROOT") && strpos($id,"bigtree-internal-") !== 0) {
 						$extension = sqlescape(rtrim(str_replace(SERVER_ROOT."extensions/","",EXTENSION_ROOT),"/"));
 						
 						// Don't append extension again if it's already being called via the namespace
