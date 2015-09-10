abfd9838aa69ba52076736bf105f01d6e6cbebb8
Tim Buckingham <mr@timbuckingham.com>
2015-09-10 13:05:11 -0400

    Updated Google-related API instructions. Added trim to API credentials for easier copy/paste.

--- core/admin/modules/developer/services/_youtube.php
+++ core/admin/modules/developer/services/_youtube.php
@@ -6,8 +6,8 @@
 	$secret_name = "Client Secret";
 	$instructions = array(
 		'Login to the <a href="https://console.developers.google.com" target="_blank">Google Developers Console</a> and create a project.',
-		'Expand "API &amp; Auth" on the left and click "APIs". Switch the "YouTube Data API v3" API toggle to ON.',
-		'Click into the "Credentials" section and click the "Create New Client ID" button.',
+		'Expand "API &amp; Auth" on the left and click "APIs". Choose the "YouTube Data API" API and enable the API.',
+		'Click into the "Credentials" section and click the "Add Credentials" button, choosing OAuth 2.0 client ID.',
 		'Choose "Web Application" for the Application Type. Enter '.DEVELOPER_ROOT.'services/youtube/return/ as the "Authorized redirect URI".',
 		'Enter the Client ID and Client Secret that was created from the previous step below.',
 		'Follow the OAuth process of allowing BigTree/your application access to your YouTube account.'
--- core/admin/modules/developer/services/common/activate.php
+++ core/admin/modules/developer/services/common/activate.php
@@ -1,6 +1,6 @@
 <?
-	$api->Settings["key"] = $_POST["key"];
-	$api->Settings["secret"] = $_POST["secret"];
+	$api->Settings["key"] = trim($_POST["key"]);
+	$api->Settings["secret"] = trim($_POST["secret"]);
 	$api->Settings["test_environment"] = $_POST["test_environment"];
 	$api->oAuthRedirect();
 ?>
