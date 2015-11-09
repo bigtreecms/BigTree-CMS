8d9e3bd310817b5a13ac4b53ea111f10a0696a8f
Tim Buckingham <mr@timbuckingham.com>
2015-11-09 14:22:32 -0500

    Fixes https://github.com/bigtreecms/BigTree-CMS/issues/219

--- core/inc/bigtree/modules.php
+++ core/inc/bigtree/modules.php
@@ -599,7 +599,7 @@
 		*/
 		
 		function getRecentFeatured($count = 5, $field = "date") {
-			return $this->fetch("$field ASC",$count,"featured = 'on' AND `$field` <= '".date("Y-m-d")."'");
+			return $this->fetch("$field DESC",$count,"featured = 'on' AND `$field` <= '".date("Y-m-d")."'");
 		}
 		
 		/*
