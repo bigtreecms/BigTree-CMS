537fe92cf4b7f6a7a73c7be0eac82c963ed5da23
Tim Buckingham <mr@timbuckingham.com>
2016-02-29 15:42:47 -0500

    Fixing file uploads for PHP 5.5 when supported.

--- core/inc/bigtree/utils.php
+++ core/inc/bigtree/utils.php
@@ -436,6 +436,16 @@
 
 			// Setup post data
 			if ($post !== false) {
+				// Use cURLFile for any file uploads
+				foreach ($post as &$post_field) {
+					if (substr($post_field,0,1) == "@" && file_exists(substr($post_field,1))) {
+						if (function_exists("curl_file_create")) {
+							$post_field = curl_file_create(substr($post_field,1));
+						}
+					}
+				}
+				unset($post_field);
+
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			}
 
