31b4ef138e2935b752b6381127bf179f07162a1a
Tim Buckingham <mr@timbuckingham.com>
2016-05-20 16:26:39 -0400

    Removing keywords from SEO tab. https://github.com/bigtreecms/BigTree-CMS/issues/227

--- core/admin/modules/pages/tabs/seo.php
+++ core/admin/modules/pages/tabs/seo.php
@@ -6,16 +6,10 @@
 	<label><?=Text::translate("URL Route")?> <small>(leave blank to auto generate)</small></label>
 	<input type="text" name="route" value="<?=$bigtree["current_page"]["route"]?>" tabindex="2" />
 </fieldset>
-<div class="contain">
-	<fieldset class="left">
-		<label><?=Text::translate("Meta Keywords")?></label>
-		<textarea name="meta_keywords"><?=$bigtree["current_page"]["meta_keywords"]?></textarea>
-	</fieldset>
-	<fieldset class="right">
-		<label><?=Text::translate("Meta Description")?></label>
-		<textarea name="meta_description"><?=$bigtree["current_page"]["meta_description"]?></textarea>
-	</fieldset>
-</div>
+<fieldset>
+	<label><?=Text::translate("Meta Description")?></label>
+	<textarea name="meta_description"><?=$bigtree["current_page"]["meta_description"]?></textarea>
+</fieldset>
 <fieldset class="last">
 	<input type="checkbox" name="seo_invisible"<?php if ($bigtree["current_page"]["seo_invisible"]) { ?> checked="checked"<?php } ?> />
 	<label class="for_checkbox"><?=Text::translate("Hide From Search Engines")?></label>
