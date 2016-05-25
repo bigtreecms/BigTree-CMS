3cbbdcc7239f875a4700b8537f0ea12929477105
Tim Buckingham <mr@timbuckingham.com>
2016-05-25 15:23:09 -0400

    Fixing Facebook employer (class didn't exist, not sure what the data response is anyway). Fixing Twitter tweet posting not properly uploading media.

--- core/inc/bigtree/apis/facebook.php
+++ core/inc/bigtree/apis/facebook.php
@@ -106,7 +106,7 @@
 			isset($user->verified) ? $this->Verified = $user->verified : false;
 			if (isset($user->work)) {
 				$this->Work = new stdClass;
-				isset($user->work["employer"]) ? $this->Work->Employer = new BigTreeFacebookEmployer($user->work["employer"],$api) : false;
+				isset($user->work["employer"]) ? $this->Work->Employer = $user->work["employer"] : false;
 				isset($user->work["location"]) ? $this->Work->Location = new BigTreeFacebookLocation($user->work["location"],$api) : false;
 				isset($user->work["position"]) ? $this->Work->JobTitle = new BigTreeFacebookJobTitle($user->work["position"],$api) : false;
 			}
--- core/inc/bigtree/apis/twitter.php
+++ core/inc/bigtree/apis/twitter.php
@@ -619,10 +619,13 @@
 
 			// Upload media first
 			if ($image) {
-				$params["media_ids"] = $this->uploadMedia($image);
+				$media_id = $this->uploadMedia($image);
+
 				if (!$media_id) {
 					return false;
 				}
+
+				$params["media_ids"] = $media_id;
 			}
 			
 			// Post tweet
