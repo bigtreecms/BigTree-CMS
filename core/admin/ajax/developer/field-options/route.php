d082a55daef0047e8f5defed353a829dd516ada8
Tim Buckingham <mr@timbuckingham.com>
2016-08-29 16:48:55 -0400

    Updating Generated Route field to support multiple source fields.

--- core/admin/ajax/developer/field-options/route.php
+++ core/admin/ajax/developer/field-options/route.php
@@ -4,11 +4,15 @@
 	$data["not_unique"] = isset($data["not_unique"]) ? $data["not_unique"] : "";
 	$data["keep_original"] = isset($data["keep_original"]) ? $data["keep_original"] : "";
 ?>
-<fieldset>
-	<label>Source Field <small>(the table column to use for route generation)</small></label>
-	<select name="source">
-		<?=BigTree::getFieldSelectOptions($_POST["table"],$data["source"])?>
-	</select>
+<fieldset id="js-source-fieldset">
+	<label>Source Fields <small>(the table columns to use for route generation)</small></label>
+	<div class="contain route_source_field">
+		<a href="#" class="icon_small icon_small_add" title="Add Another Source"></a>
+		<a href="#" class="icon_small icon_small_delete" title="Remove Source"></a>
+		<select name="source[]">
+			<?=BigTree::getFieldSelectOptions($_POST["table"],$data["source"])?>
+		</select>
+	</div>
 </fieldset>
 
 <fieldset>
@@ -20,3 +24,20 @@
 	<input type="checkbox" name="keep_original" <? if ($data["keep_original"]) { ?>checked="checked" <? } ?>/>
 	<label class="for_checkbox">Keep Original Route<small>(check to keep the first generated route)</small></label>
 </fieldset>
+
+<script>
+	$("#js-source-fieldset").on("click", ".icon_small_add", function(ev) {
+		ev.preventDefault();
+		
+		var new_field = $(".route_source_field").last().clone().attr("id", "");
+		new_field.find("select").removeClass("custom_control").prop("selectedIndex", 0);
+		new_field.find(".select").remove();
+		$("#js-source-fieldset").append(new_field);
+
+		BigTreeCustomControls("#js-source-fieldset");
+	}).on("click", ".icon_small_delete", function(ev) {
+		ev.preventDefault();
+		
+		$(this).parents(".contain").remove();
+	});
+</script>
--- core/admin/form-field-types/process/route.php
+++ core/admin/form-field-types/process/route.php
@@ -1,17 +1,31 @@
 <?
 	// If we always genereate a new route, don't have a route, or we're updating a pending entry.
 	if (!$field["options"]["keep_original"] || !$bigtree["existing_data"][$field["key"]] || (isset($bigtree["edit_id"]) && !is_numeric($bigtree["edit_id"]))) {
-		if ($field["options"]["not_unique"]) {
-			$field["output"] = $cms->urlify(strip_tags($bigtree["post_data"][$field["options"]["source"]]));
+		if (is_array($field["options"]["source"])) {
+			$source_data = "";
+			
+			foreach ($field["options"]["source"] as $source_field) {
+				$source_data .= " ".strip_tags($bigtree["post_data"][$source_field]);
+			}
 		} else {
-			$oroute = $cms->urlify(strip_tags($bigtree["post_data"][$field["options"]["source"]]));
-			$field["output"] = $oroute;
+			$source_data = strip_tags($bigtree["post_data"][$field["options"]["source"]]);
+		}
+		
+		$source_data = trim($source_data);
+		
+		if ($field["options"]["not_unique"]) {
+			$field["output"] = $cms->urlify($source_data);
+		} else {
+			$original_route = $cms->urlify($source_data);
+			$field["output"] = $original_route;
 			$x = 2;
+			
 			// We're going to try 1000 times at most so we don't time out
 			while ($x < 1000 && sqlrows(sqlquery("SELECT * FROM `".$bigtree["form"]["table"]."` WHERE `".$field["key"]."` = '".sqlescape($field["output"])."' AND id != '".sqlescape($bigtree["edit_id"])."'"))) {
-				$field["output"] = $oroute."-".$x;
+				$field["output"] = $original_route."-".$x;
 				$x++;
 			}
+			
 			if ($x == 1000) {
 				$field["output"] = "";
 			}
