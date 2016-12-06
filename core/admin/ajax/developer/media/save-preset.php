29329e7824ac3e30adb495a30af3d02be5b200c5
Tim Buckingham <mr@timbuckingham.com>
2016-12-06 16:56:45 -0500

    Clean-up code from 4.2 fix.

--- core/admin/ajax/developer/media/save-preset.php
+++ core/admin/ajax/developer/media/save-preset.php
@@ -22,7 +22,46 @@
 	$names = array();
 
 	foreach ($media_settings->Value["presets"] as $preset) {
-		$names[] = $preset["name"];
+		// Clean up empty entries
+		foreach ($preset["crops"] as $crop_index => $crop) {
+			foreach ($crop["center_crops"] as $center_crop_index => $center_crop) {
+				if (!is_array($center_crop) || !array_filter($center_crop)) {
+					unset($crop["center_crops"][$center_crop_index]);
+				}
+			}
+
+			foreach ($crop["thumbs"] as $thumb_index => $thumb) {
+				if (!is_array($thumb) || !array_filter($thumb)) {
+					unset($crop["thumbs"][$thumb_index]);
+				}
+			}
+
+			if (!array_filter($crop)) {
+				unset($preset["crops"]["crop_index"]);
+			} else {
+				$preset["crops"][$crop_index] = $crop;
+			}
+		}
+
+		foreach ($preset["thumbs"] as $thumb_index => $thumb) {
+			if (!array_filter($thumb)) {
+				unset($preset["thumbs"][$thumb_index]);
+			}
+		}
+
+		foreach ($preset["center_crops"] as $crop_index => $crop) {
+			if (!array_filter($crop)) {
+				unset($preset["center_crops"][$crop_index]);
+			}
+		}
+
+		// Only store it if this preset has stuff in it
+		if (array_filter($preset)) {
+			$names[] = $preset["name"];
+			$media_settings->Value["presets"][$index] = $preset;
+		} else {
+			unset($settings["presets"][$index]);
+		}
 	}
 
 	array_multisort($names, $media_settings->Value["presets"]);
