e2eac767cec0c0b6b044de81860debb557e3125c
Tim Buckingham <mr@timbuckingham.com>
2017-08-16 16:04:46 -0400

    Fixing some formatting.

--- core/admin/modules/users/delete.php
+++ core/admin/modules/users/delete.php
@@ -7,20 +7,20 @@
 	
 	CSRF::verify();
 	Auth::user()->requireLevel(1);
-
+	
 	$id = intval($_GET["id"]);
-
+	
 	if ($id == Auth::user()->ID) {
-		Utils::growl("Users","You cannot delete your own user.","error");
+		Utils::growl("Users", "You cannot delete your own user.", "error");
 	} else {
 		$user = new User($id);
-
+		
 		// If this person has higher access levels than the person trying to update them, fail.
 		if ($user->Level > Auth::user()->Level) {
-			Utils::growl("Users","Deleting User Failed","error");
+			Utils::growl("Users", "Deleting User Failed", "error");
 		} else {
 			$user->delete();
-			Utils::growl("Users","Deleted User");
+			Utils::growl("Users", "Deleted User");
 		}
 	}
 	
