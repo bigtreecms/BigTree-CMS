4c2cce8053901c3377167a532f1b4355a7c8506d
Tim Buckingham <mr@timbuckingham.com>
2018-02-21 11:23:58 -0500

    Fixing admin crashing when running PHP < 7.0 with the Locale class installed.

--- core/inc/bigtree/cms.php
+++ core/inc/bigtree/cms.php
@@ -1552,7 +1552,7 @@
 		*/
 
 		static function urlify($title) {
-			if (class_exists("Locale")) {
+			if (class_exists("Locale") && version_compare(PHP_VERSION, "7.0.0") >= 0) {
 				require_once(SERVER_ROOT."core/inc/lib/slug-generator/src/SlugOptions.php");
 				require_once(SERVER_ROOT."core/inc/lib/slug-generator/src/SlugGenerator.php");
 
