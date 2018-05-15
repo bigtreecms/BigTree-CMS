187a15e9e6eb2a0e386e55f6e411ce2a0f132f2d
Tim Buckingham <mr@timbuckingham.com>
2018-05-15 17:23:20 -0400

    Fixing editing callouts options/settings.

--- core/admin/modules/developer/callouts/_common-js.php
+++ core/admin/modules/developer/callouts/_common-js.php
@@ -37,13 +37,13 @@
 			var key = $(this).attr("name");
 			CurrentFieldKey = key;
 			
-			$.ajax("<?=ADMIN_ROOT?>ajax/developer/load-field-settings/", { type: "POST", data: { callout: "true", type: $("#type_" + key).val(), data: $("#options_" + key).val() }, complete: function(response) {
+			$.ajax("<?=ADMIN_ROOT?>ajax/developer/load-field-settings/", { type: "POST", data: { callout: "true", type: $("#type_" + key).val(), data: $("#settings_" + key).val() }, complete: function(response) {
 				BigTreeDialog({
 					title: "Field Options",
 					content: response.responseText,
 					icon: "edit",
 					callback: function(data) {
-						$("#options_" + CurrentFieldKey).val(JSON.stringify(data));
+						$("#settings_" + CurrentFieldKey).val(JSON.stringify(data));
 					}
 				});
 			}});
@@ -73,7 +73,7 @@
 			ResourceCount++;
 			
 			var li = $('<li id="row_' + ResourceCount + '">');
-			li.html('<section class="developer_resource_callout_id"><span class="icon_sort"></span><input type="text" name="resources[' + ResourceCount + '][id]" value="" /></section><section class="developer_resource_callout_title"><input type="text" name="resources[' + ResourceCount + '][title]" value="" /></section><section class="developer_resource_callout_subtitle"><input type="text" name="resources[' + ResourceCount + '][subtitle]" value="" /></section><section class="developer_resource_type"><select name="resources[' + ResourceCount + '][type]" id="type_' + ResourceCount + '"><optgroup label="Default"><?php foreach ($types["default"] as $k => $v) { ?><option value="<?=$k?>"><?=$v["name"]?></option><?php } ?></optgroup><?php if (count($types["custom"])) { ?><optgroup label="Custom"><?php foreach ($types["custom"] as $k => $v) { ?><option value="<?=$k?>"><?=$v["name"]?></option><?php } ?></optgroup><?php } ?></select><a href="#" tabindex="-1" class="icon_settings" name="' + ResourceCount + '"></a><input type="hidden" name="resources[' + ResourceCount + '][options]" value="" id="options_' + ResourceCount + '" /></section><section class="developer_resource_display_title"><input type="radio" name="display_field" value="" id="display_title_' + ResourceCount + '" /></section><section class="developer_resource_action right"><a href="#" tabindex="-1" class="icon_delete"></a></section>');
+			li.html('<section class="developer_resource_callout_id"><span class="icon_sort"></span><input type="text" name="resources[' + ResourceCount + '][id]" value="" /></section><section class="developer_resource_callout_title"><input type="text" name="resources[' + ResourceCount + '][title]" value="" /></section><section class="developer_resource_callout_subtitle"><input type="text" name="resources[' + ResourceCount + '][subtitle]" value="" /></section><section class="developer_resource_type"><select name="resources[' + ResourceCount + '][type]" id="type_' + ResourceCount + '"><optgroup label="Default"><?php foreach ($types["default"] as $k => $v) { ?><option value="<?=$k?>"><?=$v["name"]?></option><?php } ?></optgroup><?php if (count($types["custom"])) { ?><optgroup label="Custom"><?php foreach ($types["custom"] as $k => $v) { ?><option value="<?=$k?>"><?=$v["name"]?></option><?php } ?></optgroup><?php } ?></select><a href="#" tabindex="-1" class="icon_settings" name="' + ResourceCount + '"></a><input type="hidden" name="resources[' + ResourceCount + '][settings]" value="" id="settings_' + ResourceCount + '" /></section><section class="developer_resource_display_title"><input type="radio" name="display_field" value="" id="display_title_' + ResourceCount + '" /></section><section class="developer_resource_action right"><a href="#" tabindex="-1" class="icon_delete"></a></section>');
 	
 			$("#resource_table").append(li);
 			li.find("select").get(0).customControl = new BigTreeSelect(li.find("select").get(0));
