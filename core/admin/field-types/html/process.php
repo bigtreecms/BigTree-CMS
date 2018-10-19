9b405a336510894358201fc95d4e4f0ea962aba4
Tim Buckingham <mr@timbuckingham.com>
2018-10-19 11:20:01 -0400

    Simple HTML fields should now filter disallowed content on the back-end.

--- core/admin/field-types/html/process.php
+++ core/admin/field-types/html/process.php
@@ -1,4 +1,9 @@
 <?php
+	// For simple HTML fields, strip invalid tags
+	if (!empty($field["settings"]["simple"]) || (isset($field["settings"]["simple_by_permission"]) && $field["settings"]["simple_by_permission"] > $admin->Level)) {
+		$field["input"] = strip_tags($field["input"], "<strong><em><b><i><u><a><p><br>");
+	}
+
 	// If there are admin links, we want them stripped out and returned back to relative URLs.
 	$field["output"] = preg_replace_callback('/href="([^"]*)"/', function($matches) {
 		$href = str_replace($_SERVER["HTTP_REFERER"], "", $matches[1]);
