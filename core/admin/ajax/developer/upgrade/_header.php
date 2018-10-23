c400219fea331f2e6db84c8bc0af5934181f78bc
Tim Buckingham <mr@timbuckingham.com>
2018-10-23 18:15:13 -0400

    Only fail on warnings / errors

--- core/admin/ajax/developer/upgrade/_header.php
+++ core/admin/ajax/developer/upgrade/_header.php
@@ -3,10 +3,12 @@
 	define("BIGTREE_NO_QUERY_LOG", true);
 
 	set_error_handler(function($error_number, $error_string, $error_file, $error_line) {
-		echo BigTree::json([
-			"complete" => false,
-			"error" => $error_string
-		]);
-
-		die();
+		if ($error_number == E_USER_WARNING || $error_number == E_USER_ERROR) {
+			echo BigTree::json([
+				"complete" => false,
+				"error" => $error_string
+			]);
+			
+			die();
+		}
 	});
