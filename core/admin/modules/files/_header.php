980302bca3ac7c7d076d76c736849744e606a75f
Tim Buckingham <mr@timbuckingham.com>
2018-11-13 14:01:59 -0500

    Sorting folders by name when editing files / folders

--- core/admin/modules/files/_header.php
+++ core/admin/modules/files/_header.php
@@ -2,7 +2,7 @@
 	$recurse_folders = function($current, $parent = 0, $depth = 0) {
 		global $folder, $recurse_folders;
 
-		$folders = SQL::fetchAll("SELECT id, name FROM bigtree_resource_folders WHERE parent = ?", $parent);
+		$folders = SQL::fetchAll("SELECT id, name FROM bigtree_resource_folders WHERE parent = ? ORDER BY name ASC", $parent);
 
 		foreach ($folders as $child) {
 			if ($child["id"] != $folder["id"]) {
