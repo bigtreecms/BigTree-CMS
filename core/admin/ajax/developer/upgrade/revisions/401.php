d40e134380568dee45702cc804ec9b85752deefc
Tim Buckingham <mr@timbuckingham.com>
2018-12-03 14:55:50 -0500

    Avoid key issues, errors for missing tables

--- core/admin/ajax/developer/upgrade/revisions/401.php
+++ core/admin/ajax/developer/upgrade/revisions/401.php
@@ -17,19 +17,20 @@
 		"bigtree_templates"
 	]);
 
-	SQL::query("DROP TABLE `bigtree_callout_groups`");
-	SQL::query("DROP TABLE `bigtree_callouts`");
-	SQL::query("DROP TABLE `bigtree_extensions`");
-	SQL::query("DROP TABLE `bigtree_feeds`");
-	SQL::query("DROP TABLE `bigtree_field_types`");
-	SQL::query("DROP TABLE `bigtree_module_actions`");
-	SQL::query("DROP TABLE `bigtree_module_embeds`");
-	SQL::query("DROP TABLE `bigtree_module_forms`");
-	SQL::query("DROP TABLE `bigtree_module_groups`");
-	SQL::query("DROP TABLE `bigtree_module_reports`");
-	SQL::query("DROP TABLE `bigtree_module_views`");
-	SQL::query("DROP TABLE `bigtree_modules`");
-	SQL::query("DROP TABLE `bigtree_templates`");
+	SQL::query("SET foreign_key_checks = 0");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_callout_groups`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_callouts`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_extensions`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_feeds`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_field_types`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_module_actions`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_module_embeds`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_module_forms`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_module_groups`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_module_reports`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_module_views`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_modules`");
+	SQL::query("DROP TABLE IF EXISTS `bigtree_templates`");
 	SQL::query("ALTER TABLE `bigtree_settings` DROP COLUMN `type`");
 	SQL::query("ALTER TABLE `bigtree_settings` DROP COLUMN `settings`");
 	SQL::query("ALTER TABLE `bigtree_settings` DROP COLUMN `name`");
@@ -37,6 +38,7 @@
 	SQL::query("ALTER TABLE `bigtree_settings` DROP COLUMN `locked`");
 	SQL::query("ALTER TABLE `bigtree_settings` DROP COLUMN `system`");
 	SQL::query("ALTER TABLE `bigtree_settings` DROP COLUMN `extension`");
+	SQL::query("SET foreign_key_checks = 1");
 
 	echo BigTree::json([
 		"complete" => true,
