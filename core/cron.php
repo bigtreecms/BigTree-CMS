f3018b314aa8aab4dde299e1666d59c6624b93ff
Dmitry Chubar <dimoncds@gmail.com>
2019-03-11 11:13:47 +0300

    Re-generate sitemap.xml by cron

--- core/cron.php
+++ core/cron.php
@@ -3,7 +3,8 @@
 	include $server_root."custom/environment.php";
 	include $server_root."custom/settings.php";
 	include $server_root."core/bootstrap.php";
-	
+  include $server_root."core/inc/bigtree/sitemap.php";
+
 	$admin = new BigTreeAdmin;
 	
 	// Send out Daily Digests and Content Alerts
@@ -25,3 +26,8 @@
 	if (!$bigtree["config"]["disable_ping"]) {
 		BigTree::cURL("https://www.bigtreecms.org/ajax/ping/?www_root=".urlencode(WWW_ROOT)."&version=".urlencode(BIGTREE_VERSION));
 	}
+
+	// Re-generate sitemap.xml.
+  $sitemap = new BigTreeSitemapGenerator();
+  $xml = $sitemap->generateSitemap();
+  $sitemap->saveFile($xml);
