c5d171e2fd51cdf6cf5a53976737cfc7589f4174
Tim Buckingham <mr@timbuckingham.com>
2019-06-02 21:31:28 -0400

    The file information panel should now show when the file was last updated rather than the original creation date under "Uploaded".

--- core/admin/ajax/files/get-info.php
+++ core/admin/ajax/files/get-info.php
@@ -65,7 +65,7 @@
 	<?php
 		}
 	?>
-	<p><span><?=Text::translate("Uploaded")?></span><strong><?=str_replace(" @ ", "<br>", Auth::user()->convertTimestampTo($file["date"], Router::$Config["date_format"]." @ g:i a"))?></strong></p>
+	<p><span><?=Text::translate("Uploaded")?></span><strong><?=str_replace(" @ ", "<br>", Auth::user()->convertTimestampTo($file["file_last_updated"] ?: $file["date"], Router::$Config["date_format"]." @ g:i a"))?></strong></p>
 	<?php
 		if ($folder) {
 	?>
