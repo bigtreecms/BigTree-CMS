a08d5155de47cf050df886f060ee048f80442abf
Tim Buckingham <mr@timbuckingham.com>
2019-06-05 09:00:55 -0700

    Adding error when adding a tag that already exists. https://github.com/bigtreecms/BigTree-CMS/issues/366

--- core/admin/modules/tags/create.php
+++ core/admin/modules/tags/create.php
@@ -2,7 +2,12 @@
 	$tag_string = trim($_POST["tag"]);
 
 	if (empty($tag_string)) {
-		$admin->growl("Tags", "Tag Name Was Empty");
+		$admin->growl("Tags", "Tag Name Was Empty", "error");
+		BigTree::redirect(ADMIN_ROOT."tags/add/");
+	}
+
+	if (SQL::exists("bigtree_tags", ["tag" => strtolower(html_entity_decode($tag_string))])) {
+		$admin->growl("Tags", "Tag Already Exists", "error");
 		BigTree::redirect(ADMIN_ROOT."tags/add/");
 	}
 
