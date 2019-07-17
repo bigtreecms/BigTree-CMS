f893ef5939fa662dfc8ec79e01d24605031820f8
Tim Buckingham <mr@timbuckingham.com>
2019-07-17 16:02:12 -0400

    Fixing home not working in pages breadcrumb

--- core/admin/vue/navigation/breadcrumb.vue
+++ core/admin/vue/navigation/breadcrumb.vue
@@ -12,7 +12,7 @@
 <template>
 	<nav class="breadcrumb">
 		<span v-for="link in links" class="breadcrumb_item">
-			<a v-if="link.id" v-on:click="click" :data-id="link.id" :href="link.url" class="breadcrumb_link">{{ translate(link.title) }}</a>
+			<a v-if="typeof link.id !== 'undefined'" v-on:click="click" :data-id="link.id" :href="link.url" class="breadcrumb_link">{{ translate(link.title) }}</a>
 			<a v-else class="breadcrumb_link" :href="link.url">{{ translate(link.title) }}</a>
 		</span>
 	</nav>
