745c6c8291b038b9a41260e5270d39290a9299fd
Tim Buckingham <mr@timbuckingham.com>
2019-08-29 17:26:41 -0400

    Radio field, v-model on list.

--- core/admin/vue/field-types/list.vue
+++ core/admin/vue/field-types/list.vue
@@ -13,15 +13,15 @@
 			return {
 				current_value: this.value
 			}
-		},
+		}
 	});
 </script>
 
 <template>
 	<field :title="title" :subtitle="subtitle" :label_for="'field_' + this._uid">
 		<div class="field_select_wrapper">
-			<select :name="name" :id="'field_' + this._uid" class="field_select" :required="required">
-				<option v-for="option in options" :value="option.value" :selected="option.value === value">{{ option.title }}</option>
+			<select :name="name" :id="'field_' + this._uid" class="field_select" :required="required" v-model="current_value">
+				<option v-for="option in options" :value="option.value">{{ option.title }}</option>
 			</select>
 			<icon icon="arrow_drop_down" wrapper="field_select"></icon>
 		</div>
---
+++ core/admin/vue/field-types/radio.vue
@@ -0,0 +1,31 @@
+<script>
+	Vue.component("field-radio", {
+		props: [
+			"title",
+			"subtitle",
+			"name",
+			"options",
+			"value",
+			"required"
+		],
+
+		data: function() {
+			return {
+				current_value: this.value
+			}
+		}
+	});
+</script>
+
+<template>
+	<field :title="title" :subtitle="subtitle" legend="true">
+		<div class="field_choices">
+			<div class="field_choice" v-for="(option, index) in options">
+				<input class="field_choice_input" :name="name" :value="option.value" type="radio" :required="required"
+					   :id="'field_' + this._uid + '_' + index" v-model="current_value">
+				<span class="field_choice_indicator field_choice_indicator_radio"></span>
+				<label class="field_choice_label" :for="'field_' + this._uid + '_' + index">{{ option.title }}</label>
+			</div>
+		</div>
+	</field>
+</template>
