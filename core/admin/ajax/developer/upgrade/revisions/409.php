fee3c74280b7ae499bf1a3b252e4962a8231a1fb
Tim Buckingham <mr@timbuckingham.com>
2020-06-15 14:24:57 -0400

    Creating the OG table if it wasn't in there earlier due to the bad base SQL.

--- core/admin/ajax/developer/upgrade/revisions/409.php
+++ core/admin/ajax/developer/upgrade/revisions/409.php
@@ -1,5 +1,8 @@
 <?php
 	// BigTree 4.4.10
+
+	// If open graph doesn't exist because of the bad base SQL, create it
+	SQL::query("CREATE TABLE IF NOT EXISTS `bigtree_open_graph` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `table` varchar(255) NOT NULL DEFAULT '', `entry` int(11) unsigned NOT NULL, `type` varchar(255) DEFAULT NULL, `title` varchar(255) DEFAULT NULL, `description` text, `image` varchar(255) DEFAULT NULL, `image_width` int(11) DEFAULT NULL, `image_height` int(11) DEFAULT NULL, PRIMARY KEY (`id`), KEY `table` (`table`), KEY `entry` (`entry`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci");
 	
 	// Add indexes to open graph table
 	SQL::query("ALTER TABLE `bigtree_open_graph` ADD KEY `entry` (`entry`)");
