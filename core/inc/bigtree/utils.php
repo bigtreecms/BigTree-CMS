069db8458a9c2ea919587abbb1e0dc4a8d3e40f7
Tim Buckingham <mr@timbuckingham.com>
2020-11-08 19:50:04 -0500

    Fixing BigTree::currentURL when behind a proxy

--- core/inc/bigtree/utils.php
+++ core/inc/bigtree/utils.php
@@ -357,7 +357,8 @@
 		*/
 		
 		public static function currentURL($port = false) {
-			$protocol = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+			$protocol = static::getIsSSL() ? "https://" : "http://";
+			
 			if ($_SERVER["SERVER_PORT"] != "80" && $port) {
 				return $protocol.$_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
 			} else {
