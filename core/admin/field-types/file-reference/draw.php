62e711760826e82ffeeef6028191c150ff48758a
Tim Buckingham <mr@timbuckingham.com>
2021-05-22 17:17:31 -0400

    Fixing File Reference field losing title hint when used as the title of a matrix field

--- core/admin/field-types/file-reference/draw.php
+++ core/admin/field-types/file-reference/draw.php
@@ -12,6 +12,7 @@
 	<a href="#<?=$field["id"]?>" data-options="<?=$button_options?>" class="button resource_browser_button"><span class="icon_folder"></span>Browse</a>
 	<br class="clear" />
 	<div class="currently_file" id="<?=$field["id"]?>"<?php if (empty($field["value"])) { ?> style="display: none;"<?php } ?>>
+		<input type="hidden" value="<?=$name?>" />
 		<input type="hidden" name="<?=$field["key"]?>" value="<?=$field["value"]?>" class="js-input">
 		<strong>Currently:</strong>
 		<a href="<?=ADMIN_ROOT?>files/edit/file/<?=$field["value"]?>/" target="_blank" class="js-current-file"><?=$name?></a>
