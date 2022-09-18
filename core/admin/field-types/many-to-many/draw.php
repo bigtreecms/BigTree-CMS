8f2301345400eb8c3a590a366622bec61d0df8a8
Tim Buckingham <mr@timbuckingham.com>
2022-09-18 19:13:33 -0400

    MTM and Nested fixes

--- core/admin/field-types/many-to-many/draw.php
+++ core/admin/field-types/many-to-many/draw.php
@@ -1,4 +1,9 @@
 <?php
+	/**
+	 * @global array $bigtree
+	 * @global array $field
+	 */
+	
 	// Find out whether this is a draggable Many to Many.
 	$table_description = BigTree::describeTable($field["settings"]["mtm-connecting-table"]);
 	$cols = $table_description["columns"];
@@ -14,12 +19,13 @@
 	if (is_array($field["value"])) {
 		foreach ($field["value"] as $oid) {
 			$g = sqlfetch(sqlquery("SELECT * FROM `".$field["settings"]["mtm-other-table"]."` WHERE id = '$oid'"));
+			
 			if ($g) {
 				$entries[$g["id"]] = $g[$field["settings"]["mtm-other-descriptor"]];
 			}			
 		}
 	// No pending data, let's query the connecting table directly for the entries, but only if this isn't a new entry
-	} elseif ($bigtree["edit_id"]) {
+	} elseif (!empty($bigtree["edit_id"])) {
 		if ($sortable) {
 			$q = sqlquery("SELECT * FROM `".$field["settings"]["mtm-connecting-table"]."` WHERE `".$field["settings"]["mtm-my-id"]."` = '".$bigtree["edit_id"]."' ORDER BY `position` DESC");
 		} else {
@@ -29,6 +35,7 @@
 		while ($f = sqlfetch($q)) {
 			// Get the title from the other table.
 			$g = sqlfetch(sqlquery("SELECT * FROM `".$field["settings"]["mtm-other-table"]."` WHERE id = '".$f[$field["settings"]["mtm-other-id"]]."'"));
+			
 			if ($g) {
 				$entries[$g["id"]] = $g[$field["settings"]["mtm-other-descriptor"]];
 			}
@@ -38,6 +45,7 @@
 	// Gather a list of the items that could possibly be tagged.
 	$list = array();
 	$q = sqlquery("SELECT * FROM `".$field["settings"]["mtm-other-table"]."` ORDER BY ".$field["settings"]["mtm-sort"]);
+	
 	while ($f = sqlfetch($q)) {
 		$list[$f["id"]] = $f[$field["settings"]["mtm-other-descriptor"]];
 	}
@@ -68,12 +76,14 @@
 			foreach ($entries as $id => $description) {
 		?>
 		<li>
-			<input type="hidden" name="<?=$field["key"]?>[<?=$x?>]" value="<?=BigTree::safeEncode($id)?>" />
-			<?php if ($sortable) { ?>
-			<span class="icon_sort"></span>
-			<?php } ?>
-			<p class="multi_widget_entry_title"><?=BigTree::safeEncode(BigTree::trimLength(strip_tags($description),100))?></p>
-			<a href="#" class="icon_delete"></a>
+			<div class="inner">
+				<input type="hidden" name="<?=$field["key"]?>[<?=$x?>]" value="<?=BigTree::safeEncode($id)?>" />
+				<?php if ($sortable) { ?>
+				<span class="icon_sort"></span>
+				<?php } ?>
+				<p class="multi_widget_entry_title"><?=BigTree::safeEncode(BigTree::trimLength(strip_tags($description),100))?></p>
+				<a href="#" class="icon_delete"></a>
+			</div>
 		</li>
 		<?php
 				$x++;
@@ -92,12 +102,13 @@
 		?>
 		<small class="max">LIMIT <?=$max?></small>
 		<?php
-			} elseif ($field["settings"]["show_add_all"]) {
+			} elseif (!empty($field["settings"]["show_add_all"])) {
 		?>
 		<a href="#" class="add_all button">Add All</a>
 		<?php
 			}
-			if ($field["settings"]["show_reset"]) {
+			
+			if (!empty($field["settings"]["show_reset"])) {
 		?>
 		<a href="#" class="reset button red">Reset</a>
 		<?php
