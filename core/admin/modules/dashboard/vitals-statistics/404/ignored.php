bd1483c0e222ca76478dd0d908e811f8369010d3
Tim Buckingham <mr@timbuckingham.com>
2023-06-11 22:03:08 -0400

    Changing the export UI for 404s/301s to better accomodate large numbers of pages

--- core/admin/modules/dashboard/vitals-statistics/404/ignored.php
+++ core/admin/modules/dashboard/vitals-statistics/404/ignored.php
@@ -5,14 +5,21 @@
 ?>
 <form method="POST" action="<?=MODULE_ROOT?>export/">
 	<div class="table">
+		<div class="developer_buttons">
+			<button type="submit" title="Export 404s">
+				Export 404s
+				<span class="icon_small icon_small_export"></span>
+			</button>
+		</div>
+		
 		<summary>
 			<input type="hidden" name="type" value="ignored" />
-			<input type="submit" class="button green export" value="Export" />
 			<?php include BigTree::path("admin/modules/dashboard/vitals-statistics/404/_site-key-switcher.php"); ?>
 			<input type="search" class="form_search" placeholder="Search" id="404_search" />
 			<span class="form_search_icon"></span>
 			<nav id="view_paging" class="view_paging"></nav>
 		</summary>
+		
 		<header>
 			<span class="requests_404">Requests</span>
 			<span class="url_404">404 URL</span>
@@ -20,6 +27,7 @@
 			<span class="ignore_404">Unignore</span>
 			<span class="ignore_404">Delete</span>
 		</header>
+		
 		<ul id="results">
 			<?php include BigTree::path("admin/ajax/dashboard/404/search.php"); ?>
 		</ul>
