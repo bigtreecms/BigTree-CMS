2d2b2d603b6e99d2417f48a9cc23ea80d582f640
Tim Buckingham <mr@timbuckingham.com>
2023-09-05 18:00:42 -0400

    Adding login retry / fallback on multi-site login failures

--- core/admin/modules/login/cors.php
+++ core/admin/modules/login/cors.php
@@ -12,6 +12,14 @@
 		<fieldset class="clear">
 			<p>You are being logged into all available domains and will be redirected when the process is complete.</p>
 			<p>Logging into <span id="login-domain"><?=$domains[0]?></span>...</p>
+			
+			<div class="js-multi-login-failed" style="display: none;">
+				<p class="error_message" style="margin: 10px 0">
+					We seem to be having trouble logging into alternate domains. You can continue onto the BigTree admin but may not see the BigTree bar or have preview functionality on your alternate domains.
+				</p>
+				
+				<a class="button" href="<?=ADMIN_ROOT?>dashboard/">Continue</a>
+			</div>
 		</fieldset>
 		<br />
 	</form>
@@ -20,6 +28,7 @@
 	(function() {
 		var Domains = <?=json_encode($domains)?>;
 		var Completed = 0;
+		var Failures = 0;
 		var Total = <?=count($domains)?>;
 
 		function multiSiteLogin(index) {
@@ -36,7 +45,15 @@
 				} else {
 					multiSiteLogin(Completed);
 				}
-			});
+			}).fail((function() {
+				// Try again
+				multiSiteLogin(Completed);
+				Failures++;
+				
+				if (Failures === 10) {
+					$(".js-multi-login-failed").show();
+				}
+			}));
 		}
 
 		multiSiteLogin(0);
