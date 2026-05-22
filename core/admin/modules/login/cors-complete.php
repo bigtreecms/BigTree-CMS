288c50c6afc15e3ee01eff6b09069b53c6103f24
Tim Buckingham <mr@timbuckingham.com>
2026-05-22 17:24:57 -0400

    Fix XSS in CORS multi-site login

--- core/admin/modules/login/cors-complete.php
+++ core/admin/modules/login/cors-complete.php
@@ -1,4 +1,6 @@
 <?php
-	$cache_data = BigTreeCMS::cacheGet("org.bigtreecms.login-session", $_GET["key"]);
-	BigTreeCMS::cacheDelete("org.bigtreecms.login-session", $_GET["key"]);
-	BigTree::redirect($cache_data["login_redirect"] ?: ADMIN_ROOT);
+	$key = $_GET["key"] ?? "";
+	$cache_data = BigTreeCMS::cacheGet("org.bigtreecms.login-session", $key) ?? [];
+	
+	BigTreeCMS::cacheDelete("org.bigtreecms.login-session", $key);
+	BigTree::redirect(!empty($cache_data["login_redirect"]) ? $cache_data["login_redirect"] : ADMIN_ROOT);
